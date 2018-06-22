7d7cae29ee0bd97e3e7597287c7b7fe35fc453b1
Fabio Frazao <fsfrazao@gmail.com>
2018-06-22 12:42:00 -0300

    ENH -Import submodules into the namespace

--- sound_classification/__init__.py
+++ sound_classification/__init__.py
@@ -1,1 +1,5 @@
+import sound_classification.data_handling
+import sound_classification.pre_processing
+import sound_classification.neural_networks
+
 __all__ = ["data_handling", "pre_processing", "neural_networks"]
