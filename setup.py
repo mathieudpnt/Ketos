e8439e42a486f7971d778fce4a7cb7d27dc95543
Fabio Frazao <fsfrazao@gmail.com>
2018-06-27 08:41:07 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(name='sound_classification',
+      version='0.0.1',
+      description='MERIDIAN's internal package for sound cladssification projects',
+      long_description = readme(),
+      url='http://github.com/fsfrazao/Trees',
+      author='Oliver Kirsebom, Fabio Frazao',
+      author_email='oliver.kirsebom@dal.ca, fsfrazao@dal.ca',
+      license='GNU General Public License v3.0',
+      packages=['sound_classification'],
+      install_requires=[
+          'numpy',
+          'tables',
+          'scipy',
+          'cv2',
+          'pandas',
+          'tensorflow'
+          ],
+      setup_requires=['pytest-runner',],
+      tests_require=['pytest',],
+      include_package_data=True,
+      zip_safe=False)
