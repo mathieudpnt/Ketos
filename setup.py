37892d634a22c60434ee8bfe935bc7b5aa2ed977
Fabio Frazao <fsfrazao@gmail.com>
2018-06-27 08:43:43 -0300

    BUG - Fix double quotation in the description field and comment the call to the still undefined function 'readme()'

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup
 
 setup(name='sound_classification',
       version='0.0.1',
-      description='MERIDIAN's internal package for sound cladssification projects',
-      long_description = readme(),
+      description="MERIDIAN's internal package for sound cladssification projects",
+      # TODO: define a function readme() that reads the contents of a readme file
+      # long_description=readme(),
       url='http://github.com/fsfrazao/Trees',
       author='Oliver Kirsebom, Fabio Frazao',
       author_email='oliver.kirsebom@dal.ca, fsfrazao@dal.ca',
