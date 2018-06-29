b851efc7f536ab5f1d2da4fef489b2d7ab0abfe6
Fabio Frazao <fsfrazao@gmail.com>
2018-06-29 09:19:08 -0300

    test_pre_processing-ENH-sine_wave-> Add fixture to generate a simple sine signal
    Former-commit-id: 8eafbd7573212fb37656fb1cb2442b34a9e00486

--- sound_classification/tests/test_pre_processing.py
+++ sound_classification/tests/test_pre_processing.py
@@ -1,4 +1,16 @@
 import pytest
+import numpy as np
 import scipy.signal as sg
 import sound_classification.pre_processing as pp
 
+
+@pytest.fixture
+def sine_wave():
+    sampling_rate = 44100
+    frequency = 20
+    duration = 3
+    x = np.arange(duration * sampling_rate)
+
+    signal = 32600*np.sin(2 * np.pi * frequency * x / sampling_rate) 
+
+    return signal
