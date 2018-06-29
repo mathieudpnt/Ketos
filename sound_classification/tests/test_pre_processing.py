81f6f14b904d915898b6c1eea2902a33d4276894
Fabio Frazao <fsfrazao@gmail.com>
2018-06-29 09:59:00 -0300

    test_pre_processing-TST-sine_wave_file-> Add fixture to create a .wav file with a sine signal
    Former-commit-id: e12a92f17f3522d867270c058ed05422d45abbdc

--- sound_classification/tests/test_pre_processing.py
+++ sound_classification/tests/test_pre_processing.py
@@ -1,4 +1,5 @@
 import pytest
+import os
 import numpy as np
 import scipy.signal as sg
 import sound_classification.pre_processing as pp
@@ -26,3 +27,33 @@ def square_wave():
     signal = 32600 * sg.square(2 * np.pi * frequency * x / sampling_rate) 
 
     return signal
+
+@pytest.fixture
+def square_wave():
+    sampling_rate = 44100
+    frequency = 20
+    duration = 3
+    x = np.arange(duration * sampling_rate)
+
+    signal = 32600 * sg.square(2 * np.pi * frequency * x / sampling_rate) 
+
+    return signal
+    
+@pytest.fixture
+def sine_wave_file(sine_wave):
+    """Create a .wav with the 'sine_wave()' fixture
+    
+       The file is saved as ./assets/sine_wave.wav.
+       When the tests using this fixture are done, 
+       the file is deleted.
+
+
+       Yields:
+            wav_file : str
+                A string containing the path to the .wav file.
+    """
+    wav_file = "./assets/sine_wave.wav"
+    pp.wave.write(wav_file, rate=44100, data=sine_wave)
+
+    yield wav_file
+    os.remove(wav_file)
