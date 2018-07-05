433cf3030d1bf34d65497958b86f4615abf787fa
Fabio Frazao <fsfrazao@gmail.com>
2018-07-05 12:57:01 -0300

    neural_networks.py-DOC-Add module docstring
    Former-commit-id: ba65011ed5fa1d0f4605417b4fdd585563cbacbd
    Former-commit-id: 9b1eac52436bca27680621de020fc516e4bbfbe9

---
+++ sound_classification/neural_networks.py
@@ -0,0 +1,16 @@
+""" Neural Networks module within the sound_classification package
+
+This module includes classes and functions useful for creating 
+Deep Neural Networks applied to sound classification within MERIDIAN.
+
+Authors: Fabio Frazao and Oliver Kirsebom
+    contact: fsfrazao@dal.ca, oliver.kirsebom@dal.ca
+    Organization: MERIDIAN
+    Team: Acoustic data Analytics, Dalhousie University
+    Project: packages/sound_classification
+             Project goal: To package code useful for handling data, deriving features and 
+             creating Deep Neural Networks for sound classification projects.
+     
+    License:
+
+"""
