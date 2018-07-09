2220a6a80f9cc1239d9f049309c469d59fd9875f
Fabio Frazao <fsfrazao@gmail.com>
2018-07-09 08:41:03 -0300

    data_handling.py-DOC-to1hot->Add function docstring
    Former-commit-id: 29fc62afeaf8a4c180df6c7be4c9cd2a7fd74e9b
    Former-commit-id: bddb278e9165d03d7f756d459f25ed4d0d33c862

--- sound_classification/data_handling.py
+++ sound_classification/data_handling.py
@@ -17,6 +17,18 @@ Authors: Fabio Frazao and Oliver Kirsebom
 
 
 def to1hot(row):
+    """Converts the binary label to one hot format
+
+            Args:
+                row: bool/int(0 or 1)
+                    The the label to be converted.
+            
+            Returns:
+                one_hot:numpy array
+                    A 1 by 2 array containg [1,0] if row was 0
+                    and [0,1] if it was 1.
+     """
+
     one_hot = np.zeros(2)
     one_hot[row] = 1.0
     return one_hot
