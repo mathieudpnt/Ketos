842e95f58ad2c078eb7e1620bce705a6ef65e477
oliver.kirsebom@dal.ca <oliver.kirsebom@dal.ca>
2018-07-12 14:59:55 -0300

    test_neural_networks - TST - added header to test file
    Former-commit-id: ef41336b16afd487b4d840d0d9f65f5d9f92c3c0
    Former-commit-id: efa9e21bc2896d1c697864dc56e64c881b01ae4c

---
+++ sound_classification/tests/test_neural_networks.py
@@ -0,0 +1,19 @@
+""" Unit tests for the the 'data_handling' module in the 'sound_classification' package
+
+
+    Authors: Fabio Frazao and Oliver Kirsebom
+    contact: fsfrazao@dal.ca and oliver.kirsebom@dal.ca
+    Organization: MERIDIAN-Intitute for Big Data Analytics
+    Team: Acoustic data Analytics, Dalhousie University
+    Project: packages/sound_classification
+             Project goal: Package code internally used in projects applying Deep Learning to sound classification
+     
+    License:
+
+"""
+
+import pytest
+import numpy as np
+import pandas as pd
+import sound_classification.data_handling as dh
+import sound_classification.neural_networks as nn
