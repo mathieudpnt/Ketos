627eb71244b21d6d93f75942ad07dd87b80f456b
oliver.kirsebom@dal.ca <oliver.kirsebom@dal.ca>
2018-07-13 10:41:25 -0300

    neural_networks - TST - test init CNNWhale with default constructor
    Former-commit-id: 6cca7e5d071b55d916230346703b237a8efead05
    Former-commit-id: feb6f36fb313b494befb3e7f32aa60b5cafafbaf

--- sound_classification/tests/test_neural_networks.py
+++ sound_classification/tests/test_neural_networks.py
@@ -20,6 +20,16 @@ import sound_classification.neural_networks as nn
 
 
 @pytest.mark.test_CNNWhale
-def test_initialize_CNNWhale_with_default_args(database_prepared_for_NN):
-    network = nn.CNNWhale.from_prepared_data(database_prepared_for_NN, batch_size=1, num_channels=1, num_labels=1)
+def test_initialize_CNNWhale_with_class_method_and_default_args(database_prepared_for_NN):
+    d = database_prepared_for_NN
+    network = nn.CNNWhale.from_prepared_data(d, batch_size=1, num_channels=1, num_labels=1)
 
+def test_initialize_CNNWhale_with_default_constructor_and_default_args(database_prepared_for_NN):
+    d = database_prepared_for_NN
+    train_x = d["train_x"]
+    train_y = d["train_y"]
+    validation_x = d["validation_x"]
+    validation_y = d["validation_y"]
+    test_x = d["test_x"]
+    test_y = d["test_y"]
+    network = nn.CNNWhale(train_x, train_y, validation_x, validation_y, test_x, test_y, batch_size=1, num_channels=1, num_labels=1)
