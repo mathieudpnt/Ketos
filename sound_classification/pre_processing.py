pre_processing - BUG - fixed bug in make_magnitude_spec related to normalization of frequency axis
Former-commit-id: 059d0e9c462c2c2e3d5bf1b5d5ce475299108cfb
Former-commit-id: dbc23fdb0e366c848678f9298809ef39b5f84ddf

sound_classification/pre_processing.py
@@ -126,7 +126,7 @@ def make_magnitude_spec(sig, rate, winlen, winstep, decibel_scale=False, hamming
             mag_spec = 20 * np.log10(mag_spec)
 
     #Frequency range (Hz)
-    index_to_Hz = rate / mag_spec.shape[1]
+    index_to_Hz = rate / mag_spec.shape[1] / 2
     
     #Number of points used for FFT
     if NFFT is None:

sound_classification/tests/test_pre_processing.py
@@ -129,7 +129,7 @@ def test_make_magnitude_spec_of_sine_wave_is_delta_function(sine_wave):
     for i in range(mag.shape[0]):
         freq   = np.argmax(mag[i])
         freqHz = freq * Hz
-        assert freqHz == pytest.approx(2000, Hz)
+        assert freqHz == pytest.approx(2000, abs=Hz)
 
 @pytest.mark.test_make_magnitude_spec
 def test_make_magnitude_spec_returns_decibels(sine_wave):
@@ -151,7 +151,7 @@ def test_user_can_set_number_of_points_for_FFT(sine_wave):
     for i in range(mag.shape[0]):
         freq   = np.argmax(mag[i])
         freqHz = freq * Hz
-        assert freqHz == pytest.approx(2000, Hz)
+        assert freqHz == pytest.approx(2000, abs=Hz)
 
 @pytest.mark.test_make_magnitude_spec
 def test_make_magnitude_spec_returns_correct_NFFT_value(sine_wave):