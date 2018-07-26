1aedb579aae33647894a894da8a39626627b1fe9
oliver.kirsebom@dal.ca <oliver.kirsebom@dal.ca>
2018-07-26 13:43:44 -0300

    DOC - added docs for methods in Spectrogram class
    Former-commit-id: b024a83b1da8f7344d3c31d90b737f4d52b8b719
    Former-commit-id: 2166371cff82a0bae8602a272743c519245eef0e

--- sound_classification/spectrogram.py
+++ sound_classification/spectrogram.py
@@ -33,10 +33,36 @@ class Spectrogram():
         self.freq_max = freq_min + freq_res * image.shape[1]
 
     def _find_freq_bin(self, freq):
+        """ Find bin corresponding to given frequency in Hz
+
+            Args:
+                freq: float
+                    Frequency in Hz 
+
+            Returns:
+                bin : int
+                    Bin number
+        """
         bin = int(freq / self.freq_res)
         return bin
 
     def _crop_freq_image(self, freq_interval):
+        """ Crop image along frequency axis.
+            
+            If the frequency interval extends beyond the boarders of the image, 
+            only the overlap region is returned.
+
+            If there is no overlap between the frequency interval and the image, 
+            None is returned.
+
+            Args:
+                freq_interval: Interval
+                    Frequency interval with limits given in Hz 
+
+            Returns:
+                cropped_image : 2d numpy array
+                    Cropped image
+        """
         low = self._find_freq_bin(freq_interval.low)
         high = self._find_freq_bin(freq_interval.high)
 
@@ -50,6 +76,22 @@ class Spectrogram():
         return cropped_image
 
     def crop_freq(self, freq_interval):
+        """ Crop spectogram along frequency axis.
+            
+            If the frequency interval extends beyond the boarders of the spectrogram, 
+            only the overlap region is returned.
+
+            If there is no overlap between the frequency interval and the spectrogram, 
+            None is returned.
+
+            Args:
+                freq_interval: Interval
+                    Frequency interval with limits given in Hz 
+
+            Returns:
+                cropped_spec : Spectrogram
+                    Cropped spectrogram
+        """
         cropped_image = self._crop_freq_image(freq_interval)
 
         cropped_spec = self.__class__(cropped_image, self.NFFT, self.length, self.freq_res, freq_min=freq_interval.low, timestamp=self.timestamp)
@@ -57,9 +99,51 @@ class Spectrogram():
         return cropped_spec
 
     def average(self, freq_interval):
+        """ Compute average magnitude within specified frequency interval.
+            
+            If the frequency interval extends beyond the boarders of the spectrogram, 
+            only the overlap region is used for the computation.
+
+            If there is no overlap between the frequency interval and the spectrogram, 
+            None is returned.
+
+            Args:
+                freq_interval: Interval
+                    Frequency interval with limits given in Hz 
+
+            Returns:
+                avg : float
+                    Average magnitude
+        """
         m = self._crop_freq_image(freq_interval)
-        return np.average(m)
+
+        if m is None: 
+            return None
+
+        avg = np.average(m)
+        return avg
 
     def median(self, freq_interval):
+        """ Compute median magnitude within specified frequency interval.
+            
+            If the frequency interval extends beyond the boarders of the spectrogram, 
+            only the overlap region is used for the computation.
+
+            If there is no overlap between the frequency interval and the spectrogram, 
+            None is returned.
+
+            Args:
+                freq_interval: Interval
+                    Frequency interval with limits given in Hz 
+
+            Returns:
+                med : float
+                    Average magnitude
+        """
         m = self._crop_freq_image(freq_interval)
-        return np.median(m)
+
+        if m is None: 
+            return None
+
+        med = np.average(m)
+        return med
