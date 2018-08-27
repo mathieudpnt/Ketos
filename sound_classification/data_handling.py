4f5d82295aab70a5cad523b6fa1223240e57621d
Fabio Frazao <fsfrazao@gmail.com>
2018-08-27 14:47:33 -0300

    data_handling-ENH-create_raw_signal_table_description-> Update description. Remove 'rate' column and add 'boxes'
    Former-commit-id: 648c6bfdc6629174dca11b895437de50cfdcf4b8
    Former-commit-id: 86780b2e54a24220a08bd05de0a6950539bf7cd3

--- sound_classification/data_handling.py
+++ sound_classification/data_handling.py
@@ -425,10 +425,11 @@ def create_raw_signal_table_description(signal_rate, segment_length):
     signal_length = int(np.ceil(signal_rate * segment_length))
 
     class TableDescription(tables.IsDescription):
-            label = tables.StringCol(50)
             id = tables.StringCol(25)
-            rate = tables.Int32Col()
+            labels = tables.StringCol(100)
             signal = tables.Float32Col(shape=(signal_length))
+            boxes = tables.StringCol(100)
+
     
     return TableDescription
 
