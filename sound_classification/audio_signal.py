ENH - correct normalization constant for morlet funciont
Former-commit-id: bd9c73545a5984809810a0db7a013ad97a65b605
Former-commit-id: 14eb14d602719794e0730443aefd6b86266faa29

sound_classification/audio_signal.py
@@ -35,7 +35,7 @@ class AudioSignal:
                     Frequency of the Morlet wavelet in Hz
                 width: float
                     Width of the Morlet wavelet in seconds (sigma of the Gaussian envelope)
-                samples: float
+                samples: int
                     Length of the audio signal given as the number of samples (if no value is given, samples = 6 * width * rate)
                 height: float
                     Peak value of the audio signal
@@ -52,7 +52,7 @@ class AudioSignal:
         stop = (N-1.)/2. * dt
         start = -stop
         t = np.linspace(start, stop, N)
-        y = morlet_func(t, frequency=frequency, sigma=width, displacement=displacement, norm=False)
+        y = morlet_func(t, frequency=frequency, width=width, displacement=displacement, norm=False)
         y *= height
         
         return cls(rate=rate, data=np.array(y), tag="Morlet_f{0:.0f}Hz_s{1:.3f}s")

sound_classification/util.py
@@ -22,7 +22,7 @@ def print_octave_bands_json(band_min, band_max):
         print(endpar)
     print("]")
 
-def morlet_func(t, frequency, sigma, displacement, norm=True):
+def morlet_func(t, frequency, width, displacement, norm=True):
     """ Morlet wavelet function
 
         Args:
@@ -30,7 +30,7 @@ def morlet_func(t, frequency, sigma, displacement, norm=True):
                Time in seconds at which the function is to be evaluated
             frequency: float
                 Wavelet frequency in Hz
-            sigma: float
+            width: float
                 Wavelet width in seconds (1-sigma width of the Gaussian envelope function)
             displacement: float
                 Wavelet centroid in seconds
@@ -43,16 +43,13 @@ def morlet_func(t, frequency, sigma, displacement, norm=True):
     """
 
     w = 2 * np.pi * frequency
-    s = sigma
+    s = width
     l = displacement
     x = (t-l)/s
 
-    if w > 5:
-        y = np.exp(1j*w*x) * np.exp(-0.5*(x**2))
-    else:
-        y = (np.exp(1j*w*x) - np.exp(-0.5*(w**2))) * np.exp(-0.5*(x**2))
+    y = (np.exp(1j*w*x) - np.exp(-0.5*(w**2))) * np.exp(-0.5*(x**2))
 
     if norm:
-        y *= np.pi**-0.25 * 1. / np.sqrt(s)
+        y *= (s * np.sqrt(np.pi) * (1 + np.exp(-w**2) - 2*np.exp(-0.75*w**2)) )**-0.5
 
     return np.real(y)