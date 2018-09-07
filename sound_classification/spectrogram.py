ca0774e4d15a687c99b7f4b2eec2b1c8de5aa2fa
Fabio Frazao <fsfrazao@gmail.com>
2018-09-07 11:12:02 -0300

    spectrogram.py-CLN-Spectrogram:make_spec-> Create a make_spec in order to simplify the child classes
    Former-commit-id: 148fc7e6c0cb703b23864fac2e1e36fb6b37ccef
    Former-commit-id: 181e5f8f61fd19c36215b14acff3ba8c700b2bf5

--- sound_classification/spectrogram.py
+++ sound_classification/spectrogram.py
@@ -45,6 +45,48 @@ class Spectrogram():
         self.timestamp = None
         self.flabels = None
     
+    def make_mag_spec(self, audio_signal, winlen, winstep, hamming=True, NFFT=None, timestamp=None):
+        """ Create spectrogram from audio signal
+        
+            Args:
+                signal: AudioSignal
+                    Audio signal 
+                winlen: float
+                    Window size in seconds
+                winstep: float
+                    Step size in seconds 
+                hamming: bool
+                    Apply Hamming window
+                NFFT: int
+                    Number of points for the FFT. If None, set equal to the number of samples.
+                timestamp: datetime
+                    Spectrogram time stamp (default: None)
+
+            Returns:
+                (image, NFFT, fres):numpy.array,int, int
+                A tuple with the resulting magnitude spectrogram, the NFFT and the frequency resolution
+        """
+
+         # Make frames
+        frames = make_frames(audio_signal, winlen, winstep) 
+
+        # Apply Hamming window    
+        if hamming:
+            frames *= np.hamming(frames.shape[1])
+
+        # Compute fast fourier transform
+        image = np.abs(np.fft.rfft(frames, n=NFFT))
+
+        # Number of points used for FFT
+        if NFFT is None:
+            NFFT = frames.shape[1]
+        
+        # Frequency resolution
+        fres = audio_signal.rate / 2. / image.shape[1]
+
+        return image, NFFT, fres
+
+
     def _find_tbin(self, t):
         """ Find bin corresponding to given time.
             Returns -1, if t < t_min
