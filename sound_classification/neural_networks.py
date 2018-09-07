neural_network - BUG - fixed bug in computation of layer size
Former-commit-id: 5217b0651e5cd4f89d4a2562f1d909f2ea01572b
Former-commit-id: e597009c98a472822f0a3f399a976fca7ddca754

sound_classification/neural_networks.py
@@ -269,19 +269,21 @@ class CNNWhale():
         layer1 = self.create_new_conv_layer(x_shaped, 1, 32, [2, 8], pool_shape, name='layer1')
         layer2 = self.create_new_conv_layer(layer1, 32, 64, [30, 8], pool_shape, name='layer2')
 
-        x_after_pool = int(np.ceil(self.input_shape[0]/(pool_shape[0]*2)))
-        y_after_pool = int(np.ceil(self.input_shape[1]/(pool_shape[1]*2)))
+        x_after_pool = int(np.ceil(self.input_shape[0]/(pool_shape[0]**2)))
+        y_after_pool = int(np.ceil(self.input_shape[1]/(pool_shape[1]**2)))
         
         flattened = tf.reshape(layer2, [-1, x_after_pool * y_after_pool * 64])
 
+        n_dense = 512
+
         # setup some weights and bias values for this layer, then activate with ReLU
-        wd1 = tf.Variable(tf.truncated_normal([x_after_pool* y_after_pool * 64, 512], stddev=0.03), name='wd1')
-        bd1 = tf.Variable(tf.truncated_normal([512], stddev=0.01), name='bd1')
+        wd1 = tf.Variable(tf.truncated_normal([x_after_pool* y_after_pool * 64, n_dense], stddev=0.03), name='wd1')
+        bd1 = tf.Variable(tf.truncated_normal([n_dense], stddev=0.01), name='bd1')
         dense_layer1 = tf.matmul(flattened, wd1) + bd1
         dense_layer1 = tf.nn.relu(dense_layer1)
 
         # another layer with softmax activations
-        wd2 = tf.Variable(tf.truncated_normal([512,self.num_labels], stddev=0.03), name='wd2')
+        wd2 = tf.Variable(tf.truncated_normal([n_dense,self.num_labels], stddev=0.03), name='wd2')
         bd2 = tf.Variable(tf.truncated_normal([self.num_labels], stddev=0.01), name='bd2')
         dense_layer2 = tf.matmul(dense_layer1, wd2) + bd2
         y_ = tf.nn.softmax(dense_layer2)