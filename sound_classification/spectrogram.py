6f96b9ab2669b58a1dd4e418e01033c985eff375
Fabio Frazao <fsfrazao@gmail.com>
2018-09-07 11:17:25 -0300

    spectrogram.py-CLN-MELSpectrogram:make_mel_spec-> Simplify this method by using the make_spec method from the parent class Spectrogram
    Former-commit-id: 155f08309f2bf0519fa5645a920e10d3747e50be
    Former-commit-id: fe910b27e284339f09103a17829e21eb8459fb20

--- sound_classification/spectrogram.py
+++ sound_classification/spectrogram.py
@@ -609,25 +609,7 @@ class MelSpectrogram(Spectrogram):
            
         """
 
-       
-        # Make frames
-        frames = make_frames(audio_signal, winlen, winstep) 
-
-
-        # Apply Hamming window    
-        if hamming:
-            frames *= np.hamming(frames.shape[1])
-
-        # Compute fast fourier transform
-        image = np.abs(np.fft.rfft(frames, n=NFFT))
-        
-
-        # Number of points used for FFT
-        if NFFT is None:
-            NFFT = frames.shape[1]
-        
-        # Frequency resolution
-        fres = audio_signal.rate / 2. / image.shape[1]
+        image, NFFT, fres = self.make_spec(audio_signal, winlen, winstep, hamming, NFFT, timestamp)
         power_spec = (1.0/NFFT) * (image ** 2)
         
         low_freq_mel = 0
