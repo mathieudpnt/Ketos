a7bf857abe7670bca83c27435f9955c1570dc1fb
Fabio Frazao <fsfrazao@gmail.com>
2018-09-27 11:26:27 -0300

    data_handling-CLN-write_audio_to_h5-> Change name ('write_sig_to_h5_database')
    Former-commit-id: 93fbfe5edbc6227ad0c244db67d919b8d5740318
    Former-commit-id: a969d6e2c864f2a758efbb9b9ff8d3fe01d13585

--- sound_classification/data_handling.py
+++ sound_classification/data_handling.py
@@ -485,7 +485,7 @@ def parse_seg_name(seg_name):
 
     return (id,labels)
 
-def write_sig_to_h5_database(seg_file_name,table, pad=False, duration=None ):
+def write_audio_to_h5(seg_file_name,table, pad=False, duration=None ):
     """ Write data form .wav files containing segments into the h5 database.
 
         Args:
