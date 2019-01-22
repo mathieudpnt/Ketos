e97d4c80f4b19db307839dad161a106cc369e3c1
oliver.kirsebom@dal.ca <oliver.kirsebom@dal.ca>
2019-01-22 11:48:04 -0400

    training_data_provider - ENH - option to receive new data with equal representation of 0s and 1s
    Former-commit-id: 5606f041c41a47ed8670f6f6f207277c7e0c525c
    Former-commit-id: 69d4264d74747b68549d1f5d3cdfcf25947e8857
    Former-commit-id: cd2a0378d2325f73eda6245e214a1590bec6e103

--- sound_classification/training_data_provider.py
+++ sound_classification/training_data_provider.py
@@ -14,6 +14,7 @@ Authors: Fabio Frazao and Oliver Kirsebom
 
 import numpy as np
 import pandas as pd
+from sklearn.utils import shuffle
 
 
 class TrainingDataProvider():
@@ -34,8 +35,10 @@ class TrainingDataProvider():
                 Correct predictions with confidence below conf_cut will be kept for next iteration
             seed: int
                 Seed for random number generator
+            equal_rep: bool
+                Ensure that new samples drawn at each iteration have equal representation of 0s and 1s
     """
-    def __init__(self, x, y, randomize=False, num_samples=100, max_keep=0, conf_cut=0, seed=None):
+    def __init__(self, x, y, randomize=False, num_samples=100, max_keep=0, conf_cut=0, seed=None, equal_rep=True):
 
         N = x.shape[0]
         self.x = x
@@ -46,6 +49,8 @@ class TrainingDataProvider():
         self.conf_cut = conf_cut
         self.it = 0
         self.prev_idx = 0
+        self.equal_rep = equal_rep
+        self.seed = seed
         if seed is not None:
             np.random.seed(seed) 
 
@@ -67,7 +72,7 @@ class TrainingDataProvider():
         num_poor = len(idx_poor)
 
         # get new samples
-        idx = self._get_new(num_samples=num_samples - num_poor, randomize=self.randomize)
+        idx = self._get_new(num_samples=num_samples - num_poor, randomize=self.randomize, equal_rep=self.equal_rep)
         if num_poor > 0:
             idx = idx.union(idx_poor)
 
@@ -103,21 +108,52 @@ class TrainingDataProvider():
         idx = pd.Index(idx)
         return idx
 
-    def _get_new(self, num_samples, randomize):
+    def _get_new(self, num_samples, randomize, equal_rep):
+
+        num_0 = int(num_samples / 2)
+        num_1 = num_samples - num_0
+
         if self.randomize:
             df_new = self.df[self.df.prev == False]
-            idx = np.random.choice(df_new.index, num_samples, replace=False)
+            if self.equal_rep:
+                df_new_0 = df_new[df_new.y == 0]
+                df_new_1 = df_new[df_new.y == 1]
+                idx_0 = np.random.choice(df_new_0.index, num_0, replace=False)
+                idx_1 = np.random.choice(df_new_1.index, num_1, replace=False)
+                idx = np.concatenate((idx_0, idx_1), axis=0)
+                idx = shuffle(idx, random_state=self.seed)
+            else:
+                idx = np.random.choice(df_new.index, num_samples, replace=False)
+
             idx = pd.Index(idx)
+
         else:
             start = self.prev_idx
             stop = min(start + num_samples, self.df.shape[0])
             idx = self.df.index[start:stop]
             dn = num_samples - len(idx)
-            while dn > 0:
+            dn_0 = 0
+            dn_1 = 0
+            while (dn > 0) or (dn_0 > 0) or (dn_1 > 0):
                 stop = min(dn, self.df.shape[0])
-                idx1 = self.df.index[0:stop]
-                idx = idx.union(idx1)
+                idx_add = self.df.index[0:stop]
+                idx = idx.union(idx_add)
                 dn = num_samples - len(idx)
+                if self.equal_rep:
+                    dfi = self.df.loc[idx]
+                    dn_0 = num_0 - dfi[dfi.y == 0]
+                    dn_1 = num_1 = dfi[dfi.y == 1]
+
+            if self.equal_rep:
+                dfi = self.df.loc[idx]
+                idx_0 = dfi[dfi.y == 0].index
+                idx_1 = dfi[dfi.y == 1].index
+                idx_0 = np.random.choice(idx_0, num_0, replace=False)
+                idx_1 = np.random.choice(idx_1, num_1, replace=False)
+                idx = np.concatenate((idx_0, idx_1), axis=0)
+                idx = shuffle(idx, random_state=self.seed)
+
+            idx = pd.Index(idx)
 
             self.prev_idx = stop
 
