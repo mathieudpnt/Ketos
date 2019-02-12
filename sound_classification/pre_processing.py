f50c767b4a8582c6ccc68e9a28791558fcbc6b35
oliver.kirsebom@dal.ca <oliver.kirsebom@dal.ca>
2019-02-12 16:14:26 -0400

    pre_processing - BUG - fixed small bug in BinaryClassFramer
    Former-commit-id: 4c25830ff55fa1ef659b5558523d74a98feccb46
    Former-commit-id: a07f9279e40adebf1a124381aca13c35fc13158a
    Former-commit-id: 395634bd94c338a3cf79f49f45fe162734ea6cd3

--- sound_classification/pre_processing.py
+++ sound_classification/pre_processing.py
@@ -65,7 +65,7 @@ class BinaryClassFramer():
         self.discard_mixed = discard_mixed
 
     def eof(self):
-        res = (self.idx == len(specs) - 1)
+        res = (self.idx == len(self.specs) - 1)
         return res
 
     def get_frames(self, max_frames=10000):
