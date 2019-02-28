60c7488b06613847b9356b571b82bfe40eb46243
oliver.kirsebom@dal.ca <oliver.kirsebom@dal.ca>
2019-02-28 12:05:47 -0400

    parsing - ENH - renamed namedtuple and method, and added assertion
    Former-commit-id: 17d574c2691e376cdf425362ba9851edc7a38baf
    Former-commit-id: 79691dfef2a4cf7b6a1938c5628724155d330ee4
    Former-commit-id: 85a1c46f5794718a755c0ce1d086f2a2f15177a5
    Former-commit-id: 01610d57344fa1fb633d46e0b8b38fb8a4df6546

--- ketos/data_handling/parsing.py
+++ ketos/data_handling/parsing.py
@@ -19,8 +19,8 @@ low - Lower limit (float)
 high - Upper limit (float)''' 
 
 
-SpectrConfig = namedtuple('SpectrConfig', 'rate window_size step_size window_function low_frequency_cut high_frequency_cut')
-SpectrConfig.__doc__ = '''\
+SpectrogramConfiguration = namedtuple('SpectrogramConfiguration', 'rate window_size step_size window_function low_frequency_cut high_frequency_cut')
+SpectrogramConfiguration.__doc__ = '''\
 Configuration parameters for generation of spectrograms
 
 rate - Sampling rate in Hz (int)
@@ -31,49 +31,58 @@ low_frequency_cut - Low-frequency cut-off in Hz (float)
 high_frequency_cut - High-frequency cut-off in Hz (float)''' 
 
 
-def parse_spectrogram_config(data):
-    """ Parse configuration settings for generating spectrograms
+def parse_spectrogram_configuration(data):
+    """ Parse configuration settings for generating spectrograms.
+
+        Any setting not specified in the json string will be set 
+        to None.
 
     Args:
         data : str
             Json-format string with the configuration settings 
     
     Returns:
-        c : SpectrConfig
+        c : SpectrogramConfiguration
             Spectrogram configuration settings
     """
-    # for parsing values with units
     Q = ureg.Quantity
 
     # default values
-    rate, wsiz, step, wfun, flow, fhigh = None, None, None, None, None, None
+    rate, wsiz, step, wfun, flow, fhigh = None, None, None, None, None, None    
 
     # check that entry exists, before attempting to read
     if data.get('rate') is not None:
         rate = Q(data['rate'])
         rate = rate.m_as("Hz")
+
     if data.get('window_size') is not None:
         wsiz = Q(data['window_size'])
         wsiz = wsiz.m_as("s")
+
     if data.get('step_size') is not None:
         step = Q(data['step_size'])
         step = step.m_as("s")
+
     if data.get('window_function') is not None:
+
         for name, member in WinFun.__members__.items():
             if data['window_function'] == name:
                 wfun = member
+
         if wfun is None:
             s = ", ".join(name for name, _ in WinFun.__members__.items())
             raise ValueError("Unknown window function. Select between: "+s)
+
     if data.get('low_frequency_cut') is not None:
         flow = Q(data['low_frequency_cut'])
         flow = flow.m_as("Hz")
+
     if data.get('high_frequency_cut') is not None:
         fhigh = Q(data['high_frequency_cut'])
         fhigh = fhigh.m_as("Hz")
 
-    # return
-    c = SpectrConfig(rate, wsiz, step, wfun, flow, fhigh)    
+    c = SpectrogramConfiguration(rate, wsiz, step, wfun, flow, fhigh)    
+
     return c
 
 
@@ -90,19 +99,24 @@ def parse_frequency_bands(data):
         freq_intv: list(Interval)
             Band frequency ranges in Hz 
     """
-    # for parsing values with units
     Q = ureg.Quantity
     
     name, freq_intv = list(), list()
+
     for band in data:
+
+        assert band.get('name') is not None, 'name field is required for frequency band'
+        assert band.get('range') is not None, 'range field is required for frequency band'
+
         name.append(band['name'])
+
         low = Q(band['range'][0])
         low = low.m_as('Hz')
+
         high = Q(band['range'][1])
         high = high.m_as('Hz')
+
         intv = Interval(low, high)
         freq_intv.append(intv)
 
     return name, freq_intv
-
-    
