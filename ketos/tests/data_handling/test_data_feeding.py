c44cff732b85695d92be49d51f03a5cc1fa38e9c
Fabio Frazao <fsfrazao@gmail.com>
2019-03-04 13:37:10 -0400

    test_data_feeding-DOC->update module docstring
    Former-commit-id: 52642ef365b6df29798bd5edc8d59b46860f3927
    Former-commit-id: 8648ec8042f3b8c95c58215421c300f3b65641be
    Former-commit-id: 9484519af27d0a2e9bcae253f81f9d88e6b60b89
    Former-commit-id: b0ce27844d084298aaef38a3197d3974199e6ea6

--- ketos/tests/data_handling/test_data_feeding.py
+++ ketos/tests/data_handling/test_data_feeding.py
@@ -1,13 +1,28 @@
-""" Unit tests for the the 'training_data_provider' module in the 'sound_classification' package
+""" Unit tests for the data feeding module within the ketos library
 
+    
     Authors: Fabio Frazao and Oliver Kirsebom
-    contact: fsfrazao@dal.ca and oliver.kirsebom@dal.ca
-    Organization: MERIDIAN-Intitute for Big Data Analytics
-    Team: Acoustic data Analytics, Dalhousie University
-    Project: packages/sound_classification
-             Project goal: Package code internally used in projects applying Deep Learning to sound classification
+    Contact: fsfrazao@dal.ca, oliver.kirsebom@dal.ca
+    Organization: MERIDIAN (https://meridian.cs.dal.ca/)
+    Team: Acoustic data analytics, Institute for Big Data Analytics, Dalhousie University
+    Project: ketos
+             Project goal: The ketos library provides functionalities for handling data, processing audio signals and
+             creating deep neural networks for sound detection and classification projects.
      
-    License:
+    License: GNU GPLv3
+
+        This program is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version.
+
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 """
 
@@ -49,3 +64,27 @@ def test_get_samples(data_classified_by_nn):
     x1, y1, _ = sampler.get_samples(num_samples=2) #1,2
     assert np.all(x1 == [x[1],x[2]])    
     sampler.update_prediction_confidence(pred=p[1:3], conf=c[1:3])
+
+
+
+                >>> from tables import open_file
+                >>> from ketos.data_handling.database_interface import open
+                  
+                >>> h5 = open_file("../tests/assets/15x_same_spec.h5", 'r') # create the database handle  
+                >>> train_data = open(h5, "/train/species1")
+
+                
+                >>> train_generator = BatchGenerator(hdf5_table=train_data, batch_size=3, return_batch_ids=True) #create a batch generator 
+                >>> n_epochs = 7    
+                >>> for e in range(n_epochs):
+                ...    ids, batch_X, batch_Y = next(train_generator)
+                ...    print("epoch:{0} | instance ids:{1}, X batch shape: {2}, Y batch shape: {3}".format(e, ids, batch_X.shape, batch_Y.shape))
+                epoch:0 | instance ids:[0, 1, 2], X batch shape: (3, 2413, 201), Y batch shape: (3,)
+                epoch:1 | instance ids:[3, 4, 5], X batch shape: (3, 2413, 201), Y batch shape: (3,)
+                epoch:2 | instance ids:[6, 7, 8], X batch shape: (3, 2413, 201), Y batch shape: (3,)
+                epoch:3 | instance ids:[9, 10, 11], X batch shape: (3, 2413, 201), Y batch shape: (3,)
+                epoch:4 | instance ids:[12, 13, 14], X batch shape: (3, 2413, 201), Y batch shape: (3,)
+                epoch:5 | instance ids:[0, 1, 2], X batch shape: (3, 2413, 201), Y batch shape: (3,)
+                epoch:6 | instance ids:[3, 4, 5], X batch shape: (3, 2413, 201), Y batch shape: (3,)
+
+                >>> h5.close()
