7015130a6ae9aed49ad0aceabbc5bde2a8ec4530
Fabio Frazao <fsfrazao@gmail.com>
2019-03-04 14:35:30 -0400

    test_data_feeding-TST-Add tests for Batch generator class
    Former-commit-id: cd7b5c8a860b45f7382bc316f89a9137fce9eb32
    Former-commit-id: 115a950625511489a005cdda7cc792a8260963c5
    Former-commit-id: 49bd654c6e082c9c307b6ec0dfe74a0be161c804
    Former-commit-id: 4f14cf29940de9c689cf32d487217070f553bbe6

--- ketos/tests/data_handling/test_data_feeding.py
+++ ketos/tests/data_handling/test_data_feeding.py
@@ -29,7 +29,9 @@
 import pytest
 import numpy as np
 import pandas as pd
-from ketos.data_handling.data_feeding import TrainingDataProvider
+from tables import open_file
+from ketos.data_handling.database_interface import open as open_table
+from ketos.data_handling.data_feeding import TrainingDataProvider, BatchGenerator
 from ketos.neural_networks.neural_networks import class_confidences, predictions
 
 
@@ -66,25 +68,94 @@ def test_get_samples(data_classified_by_nn):
     sampler.update_prediction_confidence(pred=p[1:3], conf=c[1:3])
 
 
+@pytest.mark.test_BatchGenerator
+def test_one_batch():
+    """ Test if one batch has the expected shape and contents
+    """
+    h5 = open_file("../assets/15x_same_spec.h5", 'r') # create the database handle  
+    train_data = open_table(h5, "/train/species1")
 
-                >>> from tables import open_file
-                >>> from ketos.data_handling.database_interface import open
-                  
-                >>> h5 = open_file("../tests/assets/15x_same_spec.h5", 'r') # create the database handle  
-                >>> train_data = open(h5, "/train/species1")
+    five_specs = train_data[:5]['data']
+    five_boxes = train_data[:5]['boxes']
 
-                
-                >>> train_generator = BatchGenerator(hdf5_table=train_data, batch_size=3, return_batch_ids=True) #create a batch generator 
-                >>> n_epochs = 7    
-                >>> for e in range(n_epochs):
-                ...    ids, batch_X, batch_Y = next(train_generator)
-                ...    print("epoch:{0} | instance ids:{1}, X batch shape: {2}, Y batch shape: {3}".format(e, ids, batch_X.shape, batch_Y.shape))
-                epoch:0 | instance ids:[0, 1, 2], X batch shape: (3, 2413, 201), Y batch shape: (3,)
-                epoch:1 | instance ids:[3, 4, 5], X batch shape: (3, 2413, 201), Y batch shape: (3,)
-                epoch:2 | instance ids:[6, 7, 8], X batch shape: (3, 2413, 201), Y batch shape: (3,)
-                epoch:3 | instance ids:[9, 10, 11], X batch shape: (3, 2413, 201), Y batch shape: (3,)
-                epoch:4 | instance ids:[12, 13, 14], X batch shape: (3, 2413, 201), Y batch shape: (3,)
-                epoch:5 | instance ids:[0, 1, 2], X batch shape: (3, 2413, 201), Y batch shape: (3,)
-                epoch:6 | instance ids:[3, 4, 5], X batch shape: (3, 2413, 201), Y batch shape: (3,)
+    train_generator = BatchGenerator(hdf5_table=train_data, batch_size=5, return_batch_ids=True) #create a batch generator 
+    ids, X, Y = next(train_generator)
+    assert ids == [0,1,2,3,4]
+    assert X.shape == (5, 2413, 201)
+    np.testing.assert_array_equal(X, five_specs)
+    assert Y.shape == (5,)
+    np.testing.assert_array_equal(Y, five_boxes)
 
-                >>> h5.close()
+    h5.close()
+
+
+@pytest.mark.test_BatchGenerator
+def test_test_labels_as_Y():
+    """ Test if batch generator returns labels instead of boxes
+    """
+    h5 = open_file("../assets/15x_same_spec.h5", 'r') # create the database handle  
+    train_data = open_table(h5, "/train/species1")
+    
+    five_labels = train_data[:5]['labels']
+
+    train_generator = BatchGenerator(hdf5_table=train_data, y_field='labels', batch_size=5, return_batch_ids=False) #create a batch generator 
+    _, Y = next(train_generator)
+    np.testing.assert_array_equal(Y, five_labels)
+    
+
+
+@pytest.mark.test_BatchGenerator
+def test_batch_sequence_same_as_db():
+    """ Test if batches are generated with instances in the same order as they appear in the database
+    """
+    h5 = open_file("../assets/15x_same_spec.h5", 'r') #create the database handle  
+    train_data = open_table(h5, "/train/species1")
+
+
+    ids_in_db = train_data[:]['id']
+    train_generator = BatchGenerator(hdf5_table=train_data, x_field='id', batch_size=3, return_batch_ids=True) #create a batch generator 
+
+    for i in range(3):
+        ids, X, _ = next(train_generator)
+        np.testing.assert_array_equal(X, ids_in_db[i*3: i*3+3])
+        assert ids == list(range(i*3, i*3+3))
+    
+    h5.close()
+
+
+
+
+# @pytest.mark.test_BatchGenerator
+# def test_instance_order():
+#     """ Test if one batch has the expected shape and contents
+#     """
+#     h5 = open_file("../assets/15x_same_spec.h5", 'r') # create the database handle  
+#     train_data = open(h5, "/train/species1")
+
+#     five_specs = train_data[:5]['data']
+#     five_labels = train_data[:5]['labels']
+
+#     train_generator = BatchGenerator(hdf5_table=train_data, batch_size=5, return_batch_ids=True) #create a batch generator 
+#     ids, X, Y = next(train_generator)
+#     assert ids == [0,1,2,3,4]
+#     assert X.shape = (5, 2413, 201)
+#     assert X = five_specs
+#     assert Y.shape = (5,)
+#     assert Y == five_labels
+
+
+
+
+    # n_epochs = 7    
+    # for e in range(n_epochs):
+    #    ids, batch_X, batch_Y = next(train_generator)
+    #    print("epoch:{0} | instance ids:{1}, X batch shape: {2}, Y batch shape: {3}".format(e, ids, batch_X.shape, batch_Y.shape))
+    # epoch:0 | instance ids:[0, 1, 2], X batch shape: (3, 2413, 201), Y batch shape: (3,)
+    # epoch:1 | instance ids:[3, 4, 5], X batch shape: (3, 2413, 201), Y batch shape: (3,)
+    # epoch:2 | instance ids:[6, 7, 8], X batch shape: (3, 2413, 201), Y batch shape: (3,)
+    # epoch:3 | instance ids:[9, 10, 11], X batch shape: (3, 2413, 201), Y batch shape: (3,)
+    # epoch:4 | instance ids:[12, 13, 14], X batch shape: (3, 2413, 201), Y batch shape: (3,)
+    # epoch:5 | instance ids:[0, 1, 2], X batch shape: (3, 2413, 201), Y batch shape: (3,)
+    # epoch:6 | instance ids:[3, 4, 5], X batch shape: (3, 2413, 201), Y batch shape: (3,)
+
+    # >>> h5.close()
