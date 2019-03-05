7465f8e0dc642b686de7554fb43e451eb5833f7f
Fabio Frazao <fsfrazao@gmail.com>
2019-03-05 14:31:25 -0400

    test_data_feeding-TST-ActiveLearningBatchGenerator:test_fet_samples_randomize-Add test
    Former-commit-id: ede3f14d5d44f5922de059672ff44890e895dfc0
    Former-commit-id: 2c32b042a537200ffc3a126db2e46396b52468d2
    Former-commit-id: cf577d2ba2d23c14969190392255b38417a1b0a1
    Former-commit-id: b2d04007f2c94c941ff6baa651623bffacfe8074

--- ketos/tests/data_handling/test_data_feeding.py
+++ ketos/tests/data_handling/test_data_feeding.py
@@ -31,17 +31,17 @@ import numpy as np
 import pandas as pd
 from tables import open_file
 from ketos.data_handling.database_interface import open as open_table
-from ketos.data_handling.data_feeding import TrainingDataProvider, BatchGenerator
+from ketos.data_handling.data_feeding import ActiveLearningBatchGenerator, BatchGenerator
 from ketos.neural_networks.neural_networks import class_confidences, predictions
 
 
-@pytest.mark.test_TrainingDataSampler
+@pytest.mark.test_ActiveLearningBatchGenerator
 def test_get_samples(data_classified_by_nn):
     x, y, w = data_classified_by_nn
     p = predictions(w)
     c = class_confidences(w)
 
-    sampler = TrainingDataProvider(x=x, y=y, randomize=False, max_keep=0.5, conf_cut=0.5, seed=1, equal_rep=False)
+    sampler = ActiveLearningBatchGenerator(x=x, y=y, randomize=False, max_keep=0.5, conf_cut=0.5, seed=1, equal_rep=False)
 
     x1, y1, _ = sampler.get_samples(num_samples=2) #0,1
     assert np.all(x1 == x[0:2])    
@@ -66,6 +66,35 @@ def test_get_samples(data_classified_by_nn):
     x1, y1, _ = sampler.get_samples(num_samples=2) #1,2
     assert np.all(x1 == [x[1],x[2]])    
     sampler.update_prediction_confidence(pred=p[1:3], conf=c[1:3])
+
+
+
+@pytest.mark.test_ActiveLearningBatchGenerator
+def test_get_samples_randomize(data_classified_by_nn):
+    x, y, w = data_classified_by_nn
+    p = predictions(w)
+    c = class_confidences(w)
+
+    
+    sampler = ActiveLearningBatchGenerator(x=x, y=y, randomize=True, max_keep=0.5, conf_cut=0.5, seed=1, equal_rep=False)
+
+    x1, y1, _ = sampler.get_samples(num_samples=2) #3,2
+    assert np.all(x1 == np.array([3,2]))    
+    sampler.update_prediction_confidence(pred=p[[3,2]], conf=c[[3,2]])
+
+    x1, y1, _ = sampler.get_samples(num_samples=2) #1,2
+    assert np.all(x1 == np.array([1,2]))    
+    sampler.update_prediction_confidence(pred=p[[3,2]], conf=c[[3,2]])
+
+    x1, y1, _ = sampler.get_samples(num_samples=2) #2,5 (keeps one from previous iteration)
+    assert np.all(x1 ==np.array([2,5]))    
+    sampler.update_prediction_confidence(pred=[y1[0],p[4]], conf=[1.,c[4]])  # pretend network has learned #2
+
+    x1, y1, _ = sampler.get_samples(num_samples=2) #4,5 (keeps one from previous iteration)
+    assert np.all(x1 ==np.array([4,5]))    
+    sampler.update_prediction_confidence(pred=[y1[0],p[5]], conf=[1.,c[5]])  # pretend network has learned #4
+
+    
 
 
 @pytest.mark.test_BatchGenerator
@@ -238,42 +267,3 @@ def test_refresh_on_epoch_end():
         assert ids == expected_ids[epoch][2]
     
     h5.close()
-
-
-
-
-
-# @pytest.mark.test_BatchGenerator
-# def test_instance_order():
-#     """ Test if one batch has the expected shape and contents
-#     """
-#     h5 = open_file("../assets/15x_same_spec.h5", 'r') # create the database handle  
-#     train_data = open(h5, "/train/species1")
-
-#     five_specs = train_data[:5]['data']
-#     five_labels = train_data[:5]['labels']
-
-#     train_generator = BatchGenerator(hdf5_table=train_data, batch_size=5, return_batch_ids=True) #create a batch generator 
-#     ids, X, Y = next(train_generator)
-#     assert ids == [0,1,2,3,4]
-#     assert X.shape = (5, 2413, 201)
-#     assert X = five_specs
-#     assert Y.shape = (5,)
-#     assert Y == five_labels
-
-
-
-
-    # n_epochs = 7    
-    # for e in range(n_epochs):
-    #    ids, batch_X, batch_Y = next(train_generator)
-    #    print("epoch:{0} | instance ids:{1}, X batch shape: {2}, Y batch shape: {3}".format(e, ids, batch_X.shape, batch_Y.shape))
-    # epoch:0 | instance ids:[0, 1, 2], X batch shape: (3, 2413, 201), Y batch shape: (3,)
-    # epoch:1 | instance ids:[3, 4, 5], X batch shape: (3, 2413, 201), Y batch shape: (3,)
-    # epoch:2 | instance ids:[6, 7, 8], X batch shape: (3, 2413, 201), Y batch shape: (3,)
-    # epoch:3 | instance ids:[9, 10, 11], X batch shape: (3, 2413, 201), Y batch shape: (3,)
-    # epoch:4 | instance ids:[12, 13, 14], X batch shape: (3, 2413, 201), Y batch shape: (3,)
-    # epoch:5 | instance ids:[0, 1, 2], X batch shape: (3, 2413, 201), Y batch shape: (3,)
-    # epoch:6 | instance ids:[3, 4, 5], X batch shape: (3, 2413, 201), Y batch shape: (3,)
-
-    # >>> h5.close()
