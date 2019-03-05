ad746b409e8c7144f4b6be66e108e12745727167
oliver.kirsebom@dal.ca <oliver.kirsebom@dal.ca>
2019-03-05 13:55:26 -0400

    utils - TST - added tests of morlet func
    Former-commit-id: 5c442ff00ce8c337c1049d146af829e9cb513c4d
    Former-commit-id: b900ea2ad0e61ef9a1a09c627c44bd7c8e409b88
    Former-commit-id: 075f929f449fd38cbcc6118b55bf92e30155da88
    Former-commit-id: fa5447ddc9f10c306bd236d40088d74a94103c75

--- ketos/tests/test_utils.py
+++ ketos/tests/test_utils.py
@@ -27,7 +27,7 @@
 
 import pytest
 import numpy as np
-from ketos.utils import tostring
+from ketos.utils import tostring, morlet_func
 
 
 @pytest.mark.test_tostring
@@ -44,3 +44,23 @@ def test_tostring():
     box = [[1,2,3],[1,2]]
     s = tostring(box)
     assert s == '[[1,2,3],[1,2]]'
+
+@pytest.mark.test_morlet_func
+def test_morlet_func_single_time():
+    time = 0.5
+    f = morlet_func(time=time, frequency=10, width=3, displacement=0)
+    assert f == pytest.approx(0.42768108, abs=1E-5) 
+
+@pytest.mark.test_morlet_func
+def test_morlet_func_multiple_times():
+    time = np.array([-1., 0., 0.5])
+    f = morlet_func(time=time, frequency=10, width=3, displacement=0)
+    assert f[0] == pytest.approx(0.41022718, abs=1E-5) 
+    assert f[1] == pytest.approx(0.43366254, abs=1E-5) 
+    assert f[2] == pytest.approx(0.42768108, abs=1E-5) 
+
+@pytest.mark.test_morlet_func
+def test_morlet_func_with_dfdt_nonzero():
+    time = 0.5
+    f = morlet_func(time=time, frequency=10, width=3, displacement=0, dfdt=0.5)
+    assert f == pytest.approx(0.302416, abs=1E-5) 
