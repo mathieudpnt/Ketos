f6e12341d5704e1541bebc1079a9e147edd8dbbe
Fabio Frazao <fsfrazao@gmail.com>
2019-06-13 17:36:41 -0300

    inception.py-ENH-> Add InceptionBlock class
    Former-commit-id: 927b2b97d40f98ec7ee82e3bb6e487c2651f7927
    Former-commit-id: aa1e2a7b32066a05b2e1368636a4b370b799b5a6
    Former-commit-id: 2190b63fa4b305773b57d360d573b4abac336b12
    Former-commit-id: 8a554f63ff36b3ca92ba17eeb544532acff99057

--- ketos/neural_networks/inception.py
+++ ketos/neural_networks/inception.py
@@ -50,3 +50,31 @@ class ConvBatchNormRelu(tf.keras.Model):
 
         return x
 
+class InceptionBlock(tf.keras.Model):
+    def __init__(self, ch, strides=1):
+        super(InceptionBlock, self).__init__()
+
+        self.ch = ch
+        self.strides = strides
+
+        self.conv1 = ConvBatchNormRelu(ch, strides=self.strides)
+        self.conv2 = ConvBatchNormRelu(ch, kernel_size=3, strides=self.strides)
+        self.conv3_1 = ConvBatchNormRelu(ch, kernel_size=3, strides=self.stride$
+        self.conv3_2 = ConvBatchNormRelu(ch, kernel_size=3, strides=1)
+
+        self.pool = tf.keras.layers.MaxPooling2D(3, strides=1, padding='same')
+        self.pool_conv = ConvBatchNormRelu(ch, strides=self.strides)
+
+    def call(self, x, training=None):
+        x1 = self.conv1(x, training=training)
+        x2 = self.conv2(x, training=training)
+        x3_1 = self.conv3_1(x, training=training)
+        x3_2 = self.conv3_2(x3_1, training=training)
+        x4 = self.pool(x)
+        x4 = self.pool_conv(x4, training=True)
+
+        out = tf.concat([x1, x2, x3_2, x4], axis=3)
+
+        return out
+
+
