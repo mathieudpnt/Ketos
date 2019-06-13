fd5c9196517ae60ce1192b4bc37c20dcb05e224b
Fabio Frazao <fsfrazao@gmail.com>
2019-06-13 17:35:08 -0300

    inception.py-ENH-> Add module docstring and description
    Former-commit-id: c80e6990492b6322ab1aa37ff559476bbc742aa2
    Former-commit-id: 2e9913a50298f73c3cd5481bb690f40757c9001d
    Former-commit-id: f89c710f9871ad433e75b696232adb0dfbbae8dc
    Former-commit-id: 6c12e312ccd97eaf2c4fbe688bf29bd2c0f5d423

---
+++ ketos/neural_networks/inception.py
@@ -0,0 +1,36 @@
+# ================================================================================ #
+#   Authors: Fabio Frazao and Oliver Kirsebom                                      #
+#   Contact: fsfrazao@dal.ca, oliver.kirsebom@dal.ca                               #
+#   Organization: MERIDIAN (https://meridian.cs.dal.ca/)                           #
+#   Team: Data Analytics                                                           #
+#   Project: ketos                                                                 #
+#   Project goal: The ketos library provides functionalities for handling          #
+#   and processing acoustic data and applying deep neural networks to sound        #
+#   detection and classification tasks.                                            #
+#                                                                                  #
+#   License: GNU GPLv3                                                             #
+#                                                                                  #
+#       This program is free software: you can redistribute it and/or modify       #
+#       it under the terms of the GNU General Public License as published by       #
+#       the Free Software Foundation, either version 3 of the License, or          #
+#       (at your option) any later version.                                        #
+#                                                                                  #
+#       This program is distributed in the hope that it will be useful,            #
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of             #
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              #
+#       GNU General Public License for more details.                               # 
+#                                                                                  #
+#       You should have received a copy of the GNU General Public License          #
+#       along with this program.  If not, see <https://www.gnu.org/licenses/>.     #
+# ================================================================================ #
+
+""" inception sub-module within the ketos.neural_networks module
+
+    This module provides classes that implement Inception Neural Networks.
+
+    Contents:
+        SiameseCNNBranch class:
+        SiameseCNN class:
+"""
+
+import tensorflow as tf
