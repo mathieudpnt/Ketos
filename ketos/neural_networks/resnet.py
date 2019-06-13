39d843c0956bf387ba0d35f191921498314d9da0
Fabio Frazao <fsfrazao@gmail.com>
2019-06-13 17:18:41 -0300

    resnet.py-ENH->Add module docstring and description
    Former-commit-id: 192cded2a59111c1a573a51ee9c8ebbc86e3c392
    Former-commit-id: 94f5400c66ef6ec7c69ffe26f9c9c17e8eb78719
    Former-commit-id: cd75b9a15bac2c4516336529af10083632ee5361
    Former-commit-id: 80eb23d61ed94c51a612fd4657391a350da2e86a

--- ketos/neural_networks/resnet.py
+++ ketos/neural_networks/resnet.py
@@ -1,3 +1,40 @@
+# ================================================================================ #
+#   Authors: Fabio Frazao and Oliver Kirsebom                                      #
+#   Contact: fsfrazao@dal.ca, oliver.kirsebom@dal.ca                               #
+#   Organization: MERIDIAN (https://meridian.cs.dal.ca/)                           #
+#   Team: Data Analytics                                                           #
+#   Project: ketos                                                                 #
+#   Project goal: The ketos library provides functionalities for handling          #
+#   and processing acoustic data and applying deep neural networks to sound        #
+#   detection and classification tasks.                                            #
+#                                                                                  #
+#   License: GNU GPLv3                                                             #
+#                                                                                  #
+#       This program is free software: you can redistribute it and/or modify       #
+#       it under the terms of the GNU General Public License as published by       #
+#       the Free Software Foundation, either version 3 of the License, or          #
+#       (at your option) any later version.                                        #
+#                                                                                  #
+#       This program is distributed in the hope that it will be useful,            #
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of             #
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              #
+#       GNU General Public License for more details.                               # 
+#                                                                                  #
+#       You should have received a copy of the GNU General Public License          #
+#       along with this program.  If not, see <https://www.gnu.org/licenses/>.     #
+# ================================================================================ #
+
+""" resnet sub-module within the ketos.neural_networks module
+
+    This module provides classes that to implement Residual Networks (ResNets).
+
+    Contents:
+        ResNetBlock class:
+        ResNet class:
+"""
+
+import tensorflow as tf
+
 class ResNetBlock(tf.keras.Model):
     def __init__(self, channels, strides=1, residual_path=False):
         super(ResNetBlock, self).__init__()
