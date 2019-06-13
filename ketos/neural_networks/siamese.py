2b7c47c109ec921e8a50abd1250414d3ff5f1335
Fabio Frazao <fsfrazao@gmail.com>
2019-06-13 17:24:02 -0300

    siamese.py-ENH->Add SiameseCNN class
    Former-commit-id: 48e603b538e8f886481b8df6d9a702940bf83f93
    Former-commit-id: eda28e0f7d7d416bbb1025106853d8b9381512e7
    Former-commit-id: f60651094dee8bc792232c8a7ea213733a622c02
    Former-commit-id: b44010dc460d1657641a2e435d342af0f4e8ba72

--- ketos/neural_networks/siamese.py
+++ ketos/neural_networks/siamese.py
@@ -49,3 +49,27 @@ class SiameseCNNBranch(tf.keras.Model):
         branch = tf.keras.activations.sigmoid(branch)
 
         return branch
+
+class SiameseCNN(tf.keras.Model):
+    def __init__(self):
+        super(SiameseCNN, self).__init__()
+        #self.input_layer = tf.keras.Input(shape = input_shape)
+        
+        self.branch1 = SiameseCNNBranch()
+        self.branch2 = SiameseCNNBranch()
+        
+        
+        self.L1_layer = tf.keras.layers.Lambda(lambda tensors:tf.keras.backend.abs(tensors[0] - tensors[1]))
+        self.final_fully_connected = tf.keras.layers.Dense(1,activation='sigmoid',bias_initializer=tf.random_normal_initializer())
+
+
+    def call(self, input1, input2):
+       
+        branch1_in = self.branch1(input1)
+        branch2_in = self.branch1(input2)
+        #branch2_in = self.branch2(input2)
+        
+        out = self.L1_layer([branch1_in, branch2_in])
+        out = self.final_fully_connected(out)
+        
+        return out
