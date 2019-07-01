c43b6d873c30cf762d83fa080629c00180f3ffb3
Oliver Kirsebom <oliver.kirsebom@gmail.com>
2019-07-01 12:14:23 -0300

    annotation - TST - implemented some unit tests
    Former-commit-id: b9e7cdbb6ebc295763a932d0ab6306c1fa8f52d5
    Former-commit-id: c9ef72790de8e32e2d83fa15caf91fdb917c7d6c
    Former-commit-id: caa43799e4788eddb23ae142814df0652d267c31
    Former-commit-id: b1662a51da3c6ad129af8fa144f3d3a95ec8da52

--- ketos/tests/audio_processing/test_annotation_new.py
+++ ketos/tests/audio_processing/test_annotation_new.py
@@ -28,10 +28,112 @@
 """
 
 import pytest
+import unittest
 import numpy as np
-from ketos.audio_processing.annotation_new import AnnotationHandler
+import pandas as pd
+from ketos.audio_processing.annotation_new import AnnotationHandler, AudioSourceHandler
 
 
-def test_initialize_empty_annotation_handler():
-    _ = AnnotationHandler()
+def test_empty_annotation_handler_has_correct_columns():
+    handler = AnnotationHandler()
+    a = handler.get()
+    unittest.TestCase().assertCountEqual(list(a.columns), ['start', 'stop', 'low', 'high', 'label'])
 
+def test_add_individual_annotations():
+    handler = AnnotationHandler()
+    # add simple annotation
+    handler.add(start=0.0, stop=4.1, label=1)
+    a = handler.get()
+    assert len(a) == 1
+    assert a['start'][0] == 0.0
+    assert a['stop'][0] == 4.1
+    assert np.isnan(a['low'][0])
+    assert np.isnan(a['high'][0])
+    assert a['label'][0] == 1
+    # add annotation with SI units
+    handler.add(start='2min', stop='5min', label=2)
+    a = handler.get()
+    assert len(a) == 2
+    assert a['start'][1] == 120
+    assert a['stop'][1] == 300
+    assert np.isnan(a['low'][1])
+    assert np.isnan(a['high'][1])
+    assert a['label'][1] == 2
+    # add annotation with frequency range
+    handler.add(start='2min', stop='5min', low='200Hz', high=3000, label=3)
+    a = handler.get()
+    assert len(a) == 3
+    assert a['low'][2] == 200
+    assert a['high'][2] == 3000
+
+def test_add_annotations_as_dataframe():
+    handler = AnnotationHandler()
+    df = pd.DataFrame({'start':[1,2], 'stop':[7,9], 'label':[14,11]})
+    handler.add(df=df)
+    a = handler.get()
+    assert len(a) == 2
+    assert a['start'][0] == 1
+    assert a['stop'][0] == 7
+    assert a['start'][1] == 2
+    assert a['stop'][1] == 9
+
+def test_add_annotations_as_dict():
+    handler = AnnotationHandler()
+    df = {'start':1, 'stop':7, 'label':14}
+    handler.add(df=df)
+    a = handler.get()
+    assert len(a) == 1
+    assert a['start'][0] == 1
+    assert a['stop'][0] == 7
+
+def test_crop_annotations_along_time_axis():
+    handler = AnnotationHandler()
+    handler.add(1, 3, 0, 100, 1)
+    handler.add(3, 5, 0, 100, 2)
+    handler.add(5, 7, 0, 100, 3)
+    handler.add(8, 10, 0, 100, 4)
+    a = handler.get()
+    assert len(a) == 4
+    handler.crop(4, 9)
+    a = handler.get()
+    assert len(a) == 3
+    assert np.array_equal(a['start'], [0, 1, 4]) 
+    assert np.array_equal(a['stop'], [1, 3, 5]) 
+    assert np.array_equal(a['label'], [2, 3, 4]) 
+
+
+    ### CONTINUE HERE ###
+
+
+def test_segment_annotations():
+    handler = AnnotationHandler()
+    handler.add(0.2, 1.1, 0, 100, 1)
+    handler.add(3.1, 4.7, 0, 100, 2)
+    ann = handler.segment(num_segs=20, window_size=1.0, step_size=0.5)
+    for x in ann:
+        print('-----------------------------')
+        print(x)
+        print(ann[x].get())
+
+def test_add_audio_tags():
+    tracker = AudioSourceHandler()
+    tracker.add(start=0.0, stop=4.1, label='test1.wav')
+    tracker.add(4.1, 8.3, 'test2.wav', offset=700.)
+    print(tracker.get())
+
+def test_crop_audio_tags():
+    tracker = AudioSourceHandler()
+    tracker.add(start=0.0, stop=4.1, label='test1.wav')
+    tracker.add(4.1, 8.3, 'test2.wav', offset=700.)
+    tracker.crop(2.0, 10.0)
+    print(tracker.get())
+
+def test_segment_audio_tags():
+    tracker = AudioSourceHandler()
+    tracker.add(start=0.0, stop=4.1, label='test1.wav')
+    tracker.add(4.1, 8.3, 'test2.wav', offset=700.)
+    ann = tracker.segment(num_segs=20, window_size=1.0, step_size=0.5)
+    for x in ann:
+        print('-----------------------------')
+        print(x)
+        print(ann[x].get())
