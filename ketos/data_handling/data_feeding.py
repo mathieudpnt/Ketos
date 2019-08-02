2d6d556c82d53510f57fdc30d6d477f60b77becb
fsfrazao <fsfrazao@dal.ca>
2019-08-02 12:08:39 -0300

    data_feeding-ENH-batchGenerator::__get_batch)indices__-> Change so the last batch contains 'batch_size + any exceeding instances, instead of only the extra instances
    Former-commit-id: 4af7a6b73ff5ac14944e516421ffe94da9a680a5
    Former-commit-id: 03acde0249bddd7dc86b897ca83e02484e5ff4e8
    Former-commit-id: 2ca577022e3fb6f3a0a42c1090c841fd8bfd757e
    Former-commit-id: 7d56601ee18b9a584af228010d3af847601bcd52

--- ketos/data_handling/data_feeding.py
+++ ketos/data_handling/data_feeding.py
@@ -222,14 +222,15 @@ class BatchGenerator():
         
         """
         ids = self.entry_indices
-        n_complete_batches = int(self.n_instances // self.batch_size) # number of batches that can accomodate self.batch_size intances
-        last_batch_size = self.n_instances % n_complete_batches
+        n_complete_batches = int( self.n_instances // self.batch_size) # number of batches that can accomodate self.batch_size intances
+        extra_instances = self.n_instances % n_complete_batches
     
-        list_of_indices = [list(ids[(i*self.batch_size):(i*self.batch_size)+self.batch_size]) for i in range(self.n_batches)]
-        if last_batch_size > 0:
-            last_batch_ids = list(ids[-last_batch_size:])
-            list_of_indices.append(last_batch_ids)
-
+        list_of_indices = [list(ids[(i*self.batch_size):(i*self.batch_size)+self.batch_size]) for i in range(n_complete_batches)]
+        if extra_instances > 0:
+            #last_batch_size = self.batch_size + extra_instances
+            extra_instance_ids = list(ids[-extra_instances:])
+            list_of_indices[-1]=list_of_indices[-1] + extra_instance_ids
+        
         return list_of_indices
 
     def __iter__(self):
