94508b63994440028707d18e5b753c5fde1b1246
fsfrazao <fsfrazao@dal.ca>
2019-09-16 13:37:40 -0300

    metrics.py=ENH-precision_recall_accuracy_f-> add function to metrics module
    Former-commit-id: a7da47357d46e64aabbfdc8a564c7de23e234f56
    Former-commit-id: 35f121c73a51e6b3e49d0d642700c32d39bbf5b9
    Former-commit-id: 8a87041e9f6dd95f39a58777809d3dd49d1f0a38
    Former-commit-id: 5f31b715d7d491ea1865dcff51fa94dd336acccc

---
+++ ketos/neural_networks/metrics.py
@@ -0,0 +1,21 @@
+from sklearn.metrics import  accuracy_score, recall_score, average_precision_score, fbeta_score
+import numpy as np
+
+def precision_recall_accuracy_f(y_true, y_pred, f_beta=1.0):
+    y_true = np.argmax(y_true, axis=1)
+    y_pred = np.argmax(y_pred, axis=1)
+    
+    epsilon = 0.000001
+    
+    a = accuracy_score(y_true, y_pred)
+    p = average_precision_score(y_true, y_pred)
+    r = recall_score(y_true, y_pred)
+
+    f = (1.0 +f_beta**2)*p*r / ((f_beta**2*p)+r+epsilon)
+  
+    precision=p
+    recall=r
+    f_score=f
+    accuracy=a
+
+    return {"f_score":f_score, "precision":precision,"recall":recall, "accuracy":accuracy}
