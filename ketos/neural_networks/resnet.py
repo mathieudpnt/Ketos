f2864bdf9a3999a5c925d87f2c9f7d95bf938d30
fsfrazao <fsfrazao@dal.ca>
2019-09-16 13:39:09 -0300

    resnet.py-ENH-Update ResNet blocks and architecture, modify printing stetements in training loop
    Former-commit-id: d25f94032dd222fff7f431fbae60955a2df68b99
    Former-commit-id: 77e010b752b662cd7e5b71f2aa5c0f749ad327d7
    Former-commit-id: ba4c40904277701c205758af69a8b6371e26a81f
    Former-commit-id: 6fb282bfdb2200affd19e670d204dc6a41b90452

--- ketos/neural_networks/resnet.py
+++ ketos/neural_networks/resnet.py
@@ -35,6 +35,8 @@
 
 import tensorflow as tf
 import os
+from .losses import FScoreLoss
+from .metrics import precision_recall_accuracy_f
 
 class ResNetBlock(tf.keras.Model):
     def __init__(self, channels, strides=1, residual_path=False):
@@ -57,6 +59,8 @@ class ResNetBlock(tf.keras.Model):
                                                 padding="same", use_bias=False,
                                                 kernel_initializer=tf.random_normal_initializer())
             self.batch_norm_down = tf.keras.layers.BatchNormalization()
+        
+        self.dropout = tf.keras.layers.Dropout(0.0)
 
     def call(self,inputs, training=None):
         residual = inputs
@@ -64,17 +68,21 @@ class ResNetBlock(tf.keras.Model):
         x = self.batch_norm_1(inputs, training=training)
         x = tf.nn.relu(x)
         x = self.conv_1(x)
+        x = self.dropout(x)
         x = self.batch_norm_2(x, training=training)
         x = tf.nn.relu(x)
         x = self.conv_2(x)
+        x = self.dropout(x)
 
         if self.residual_path:
             residual = self.batch_norm_down(inputs, training=training)
             residual = tf.nn.relu(residual)
             residual = self.conv_down(residual)
+            x = self.dropout(x)
 
         x = x + residual
         return x
+
 
 class ResNetArch(tf.keras.Model):
 
@@ -114,7 +122,8 @@ class ResNetArch(tf.keras.Model):
         self.batch_norm_final = tf.keras.layers.BatchNormalization()
         self.average_pool = tf.keras.layers.GlobalAveragePooling2D()
         self.fully_connected = tf.keras.layers.Dense(self.n_classes)
-
+        self.softmax = tf.keras.layers.Softmax()
+    
     def call(self, inputs, training=None):
 
         output = self.conv_initial(inputs)
@@ -124,8 +133,10 @@ class ResNetArch(tf.keras.Model):
         output = tf.nn.relu(output)
         output = self.average_pool(output)
         output = self.fully_connected(output)
+        output = self.softmax(output)
 
         return output
+
 
 
 class ResNetInterface():
@@ -162,10 +173,10 @@ class ResNetInterface():
         args = loss_function['parameters']
 
         built_metrics = []
-        for m in metrics 
-        if m['name'] not in cls.valid_metrics.keys():
-            raise ValueError("Invalid metric name '{}'".format(m['name']))
-        built_metrics.appen(cls.valid_loss[name])
+        for m in metrics: 
+            if m['name'] not in cls.valid_metrics.keys():
+                raise ValueError("Invalid metric name '{}'".format(m['name']))
+        built_metrics.append(cls.valid_loss[name])
 
         return built_metrics
 
@@ -205,7 +216,7 @@ class ResNetInterface():
 
         self.model=ResNetArch(block_list=block_list, n_classes=n_classes, initial_filters=initial_filters)
         self.compile_model()
-        self.metrics_names = self.model.metrics_names
+        #self.metrics_names = self.model.metrics_names
 
         
         self.log_dir = None
@@ -219,6 +230,7 @@ class ResNetInterface():
         self.model.compile(optimizer=self.optimizer,
                             loss = self.loss_function,
                             metrics = self.metrics)
+        self.metrics_names = self.model.metrics_names
 
     def set_train_generator(self, train_generator):
         self.train_generator = train_generator
@@ -243,6 +255,7 @@ class ResNetInterface():
         
     def print_metrics(self, metric_values):
         message  = [self.metrics_names[i] + ": {} ".format(metric_values[i]) for i in range(len(self.metrics_names))]
+        #import pdb; pdb.set_trace()
         print(''.join(message))
 
     def name_logs(self, logs, prefix="train_"):
@@ -254,14 +267,50 @@ class ResNetInterface():
     def train_loop(self, n_epochs, verbose=True, validate=True, log_tensorboard=True):
         for epoch in range(n_epochs):
             #Reset the metric accumulators
+            train_precision = 0    
+            train_recall = 0
+            train_f_score = 0
+            train_accuracy = 0
+            val_precision = 0    
+            val_recall = 0
+            val_f_score = 0
+            val_accuracy = 0
             self.model.reset_metrics()
                 
             for train_batch_id in range(self.train_generator.n_batches):
                 train_X, train_Y = next(self.train_generator)  
                 train_result = self.model.train_on_batch(train_X, train_Y)
+
+                train_set_pred = self.model.predict(train_X)
+                train_f_p_r = precision_recall_accuracy_f(y_true=train_Y, y_pred=train_set_pred, f_beta=0.5)
+
+                train_f_score += train_f_p_r['f_score']
+                train_precision += train_f_p_r['precision']
+                train_recall += train_f_p_r['recall']
+                train_accuracy += train_f_p_r['accuracy']
+
                 if verbose == True:
-                    print("train: ","Epoch:{} - batch:{}".format(epoch, train_batch_id))
-                    self.print_metrics(train_result)
+                    print("train: ","Epoch:{} Batch:{}".format(epoch, train_batch_id))
+                    print("loss:{:.3f} accuracy:{:.3f} precision:{:.3f} recall:{:.3f} f-score:{:.3f}".format(
+                        1-train_f_p_r['f_score'], train_f_p_r['accuracy'], train_f_p_r['precision'], train_f_p_r['recall'], train_f_p_r['f_score']) 
+                    )
+                    print("")
+
+
+                
+                    #self.print_metrics(train_result)
+            train_precision = train_precision / self.train_generator.n_batches
+            train_recall = train_recall / self.train_generator.n_batches
+            train_f_score = train_f_score / self.train_generator.n_batches
+            train_accuracy = train_accuracy / self.train_generator.n_batches
+            
+            if verbose == True:
+                    print("train: ","Epoch:{}".format(epoch))
+                    print("loss:{:.3f} accuracy:{:.3f} precision:{:.3f} recall:{:.3f} f-score:{:.3f}".format(
+                        train_result[0], train_accuracy, train_precision, train_recall, train_f_score) 
+                    )
+                    
+
             if log_tensorboard == True:
                 self.tensorboard_callback.on_epoch_end(epoch, name_logs(train_result, "train_"))                
             if validate == True:
@@ -270,16 +319,42 @@ class ResNetInterface():
                     val_result = self.model.test_on_batch(val_X, val_Y, 
                                                 # return accumulated metrics
                                                 reset_metrics=False)
+                    
+
+                    val_set_pred = self.model.predict(val_X)
+                    val_f_p_r = precision_recall_accuracy_f(y_true=val_Y, y_pred=val_set_pred, f_beta=0.5)
+
+                    val_f_score += val_f_p_r['f_score']
+                    val_precision += val_f_p_r['precision']
+                    val_recall += val_f_p_r['recall']
+                    val_accuracy += val_f_p_r['accuracy']
+
+
+                
+                    #self.print_metrics(val_result)
+                val_precision = val_precision / self.val_generator.n_batches
+                val_recall = val_recall / self.val_generator.n_batches
+                val_f_score = val_f_score / self.val_generator.n_batches
+                val_accuracy = val_accuracy / self.val_generator.n_batches
+                
                 if verbose == True:
-                    print("\nval: ")
-                    self.print_metrics(val_result)
+                        print("val: ")
+                        print("loss:{:.3f} accuracy:{:.3f} precision:{:.3f} recall:{:.3f} f-score:{:.3f}".format(
+                            val_result[0], val_accuracy, val_precision, val_recall, val_f_score) 
+                        )
+                        
+
+
+                # if verbose == True:
+                #     print("\nval: ")
+                #     self.print_metrics(val_result)
                 if log_tensorboard == True:
                     self.tensorboard_callback.on_epoch_end(epoch, name_logs(val_result, "val_"))  
 
             
-            if epoch % 5:
-                checkpoint_name = "cp-{:04d}.ckpt".format(epoch)
-                self.model.save_weights(os.path.join(self.checkpoint_dir, checkpoint_name))
+            # if epoch % 5:
+            #     checkpoint_name = "cp-{:04d}.ckpt".format(epoch)
+            #     self.model.save_weights(os.path.join(self.checkpoint_dir, checkpoint_name))
         
         if log_tensorboard == True:
             self.tensorboard_callback.on_train_end(None)
