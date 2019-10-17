spectrogram - BUG - fixed incorrect computation of number of segs
Former-commit-id: b3df96c3b96c9b22002da1093dbeaa0aa3d4b991
Former-commit-id: d897444d67a4f4d27678d29d42a537995304fa0d
Former-commit-id: 2003717b2d4274cdc7ae04ea4287c407472925e8
Former-commit-id: 01a0ee5e52ee8caf98e2ae74d15c3474ad5229e2

ketos/data_handling/database_interface.py
@@ -805,7 +805,7 @@ def create_spec_database(output_file, input_dir, annotations_file=None,\
             duration = file_duration
             num_segs = 1
         else:
-            num_segs = int(np.ceil(file_duration / duration))
+            num_segs = int(np.ceil(file_duration / (duration - overlap)))
 
         assert overlap < duration, 'Overlap must be less than duration'
 