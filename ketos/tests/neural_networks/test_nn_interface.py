327d73597f0771cee67ed9534e074bc92ca3849d
fsfrazao <fsfrazao@dal.ca>
2020-01-13 16:49:38 -0400

    nn_interface-TST-NNInterface::to1hot-> Add test
    Former-commit-id: bcf6ef40cfcff9e24413f84b88b5bd2ea363a31d
    Former-commit-id: eed3547f72c4756849d13acf2f63f2831e3a6009
    Former-commit-id: f78664e0252ebb32f3feb465e215e3f749e8639d
    Former-commit-id: 104afc2a0b22bc161fe442d611aa817a157f9447

---
+++ ketos/tests/neural_networks/test_nn_interface.py
@@ -0,0 +1,14 @@
+import pytest
+import numpy as np
+from ketos.neural_networks.nn_interface import RecipeCompat, NNInterface
+
+
+@pytest.mark.parametrize("class_label,n_classes,expected",[
+    (0,2,np.array([1.0,0.0])),
+    (1,2,np.array([0.0,1.0])),
+    (1,3,np.array([0.0,1.0, 0.0])),
+    (1,5,np.array([0.0,1.0, 0.0, 0.0, 0.0])),
+    ])
+def test_to1hot(class_label, n_classes, expected):
+    encoded =  NNInterface.to1hot(class_label, n_classes) 
+    assert(encoded == expected).all()
