446cbfb7c3af52b97ac93a2d2918c651fbc776aa
Oliver Kirsebom <oliver.kirsebom@gmail.com>
2020-01-23 18:37:11 -0400

    time_data - ENH - new module
    Former-commit-id: cd4f3c745c5b3149ff94780f81977fdb7b8d91da
    Former-commit-id: bb449fc3141204a087c832ef09fb0ed460471327
    Former-commit-id: fd6188ce86294871a8c28ddd900cafa712a08408
    Former-commit-id: 67157739a82d07bc022b1fac5f675cd9b9d85106

--- ketos/audio_processing/time_data.py
+++ ketos/audio_processing/time_data.py
@@ -24,39 +24,79 @@
 #       along with this program.  If not, see <https://www.gnu.org/licenses/>.     #
 # ================================================================================ #
 
-""" audio module within the ketos library
+""" time_data module within the ketos library
 
-    This module provides utilities to work with audio data.
+    This module provides utilities to work with time series data.
 
     Contents:
-        AudioSignal class
+        TimeData class
 """
 import os
 import numpy as np
-import librosa
-import scipy.io.wavfile as wave
-from scipy import interpolate
-import matplotlib.pyplot as plt
 import ketos.audio_processing.audio_processing as ap
-from ketos.utils import ensure_dir
-from ketos.data_handling.data_handling import read_wave
 from ketos.audio_processing.annotation import AnnotationHandler
-from ketos.utils import morlet_func
 from ketos.audio_processing.axis import LinearAxis
 
+def stack_attrs(filename, offset, label, mul):
+    """ Ensure that data attributes have expected multiplicity.
 
-class AudioSignal():
-    """ Audio signal
+        If the attribute is specified as a list or an array-like object, 
+        assert that the length equals the data multiplicity.
 
         Args:
-            rate: float
-                Sampling rate in Hz
+            filename: str or list(str)
+                Filename attribute.
+            offset: float or array-like
+                Offset attribute.
+            label: int or array-like
+                Label attribute.
+            mul: int
+                Audio/spectrogram multiplicity
+
+        Returns:
+            filename: list(str)
+                Filename attribute
+            offset: array-like
+                Offset attribute
+            label: array-like
+                Label attribute
+    """
+    if filename:
+        if isinstance(filename, str):
+            filename = [filename for _ in range(mul)]
+
+        assert len(filename) == mul, 'Number of filenames ({0}) does not match data multiplicity ({1})'.format(len(filename), mul)
+
+    if offset:
+        if isinstance(offset, float) or isinstance(offset, int):
+            offset = np.ones(mul, dtype=float) * float(offset)
+
+        assert len(offset) == mul, 'Number of offsets ({0}) does not match data multiplicity ({1})'.format(len(offset), mul)
+
+    if label:
+        if isinstance(label, float) or isinstance(label, int):
+            label = np.ones(mul, dtype=int) * int(label)
+
+        assert len(label) == mul, 'Number of labels ({0}) does not match data multiplicity ({1})'.format(len(label), mul)
+
+    return filename, offset, label
+
+class TimeData():
+    """ Parent class for time-series data classes such as
+        :class:`audio_processing.audio_signal.AudioSignal` 
+        and :class:`audio_processing.spectrogram.Spectrogram`.
+
+        Args:
             data: numpy array
-                Audio data 
+                Data
+            time_res: float
+                Time resolution in seconds
+            ndim: int
+                Dimensionality of data.
             filename: str
-                Filename of the original audio file, if available (optional)
+                Filename of the original data file, if available (optional)
             offset: float
-                Position within the original audio file, in seconds 
+                Position within the original data file, in seconds 
                 measured from the start of the file. Defaults to 0 if not specified.
             label: int
                 Spectrogram label. Optional
@@ -64,255 +104,66 @@ class AudioSignal():
                 AnnotationHandler object. Optional
 
         Attributes:
-            rate: float
-                Sampling rate in Hz
-            data: 1numpy array
-                Audio data 
+            data: numpy array
+                Data 
+            ndim: int
+                Dimensionality of data.
             time_ax: LinearAxis
                 Axis object for the time dimension
             filename: str
-                Filename of the original audio file, if available (optional)
+                Filename of the original data file, if available (optional)
             offset: float
-                Position within the original audio file, in seconds 
+                Position within the original data file, in seconds 
                 measured from the start of the file. Defaults to 0 if not specified.
             label: int
-                Spectrogram label.
+                Data label.
             annot: AnnotationHandler
                 AnnotationHandler object.
     """
-    def __init__(self, rate, data, filename='', offset=0, label=None, annot=None):
-        self.rate = rate
-        self.data = data.astype(dtype=np.float32)
-        length = data.shape[0] / rate
+    def __init__(self, data, time_res, ndim=1, filename='', offset=0, label=None, annot=None):
+        self.ndim = ndim
+        self.data = data
+        length = data.shape[0] * time_res
         self.time_ax = LinearAxis(bins=data.shape[0], extent=(0., length), label='Time (s)') #initialize time axis
 
-        if np.ndim(data) == 2:
-            mul = data.shape[1]
-            filename, offset, label = ap.stack_audio_attrs(filename, offset, label, mul)
+        if np.ndim(data) == ndim + 1: #stacked data arrays
+            mul = data.shape[ndim]
+            filename, offset, label = stack_attrs(filename, offset, label, mul)
             if annot:
-                assert annot.num_sets() == mul, 'Number of annotation sets ({0}) does not match spectrogram multiplicity ({1})'.format(annot.num_sets(), mul)
+                assert annot.num_sets() == mul, 'Number of annotation sets ({0}) does not match number of data sets ({1})'.format(annot.num_sets(), mul)
 
         self.filename = filename
         self.offset = offset
         self.label = label
         self.annot = annot
 
-    @classmethod
-    def from_wav(cls, path, channel=0, rate=None, offset=0, duration=None, resample_method='scipy'):
-        """ Load audio data from wave file.
-
-            Args:
-                path: str
-                    Path to input wave file
-                channel: int
-                    In the case of stereo recordings, this argument is used 
-                    to specify which channel to read from. Default is 0.
-                rate: float
-                    Desired sampling rate in Hz. If None, the original sampling rate will be used
-                offset: float
-                    Position within the original audio file, in seconds 
-                    measured from the start of the file. Defaults to 0 if not specified.
-                duration: float
-                    Length in seconds.
-                resample_method: str
-                    Resampling method. Only relevant if `rate` is specified. Options are
-                        * kaiser_best
-                        * kaiser_fast
-                        * scipy (default)
-                        * polyphase
-                    See https://librosa.github.io/librosa/generated/librosa.core.resample.html 
-                    for details on the individual methods.
-
-            Returns:
-                Instance of AudioSignal
-                    Audio signal
-
-            Example:
-                >>> from ketos.audio_processing.audio import AudioSignal
-                >>> # read audio signal from wav file
-                >>> a = AudioSignal.from_wav('ketos/tests/assets/grunt1.wav')
-                >>> # show signal
-                >>> fig = a.plot()
-                >>> fig.savefig("ketos/tests/assets/tmp/audio_grunt1.png")
-
-                .. image:: ../../../../ketos/tests/assets/tmp/audio_grunt1.png
-        """
-        rate_orig = librosa.get_samplerate(path)
-        start = ap.num_samples(offset, rate_orig)
-        if duration:
-            stop = ap.num_samples(offset + duration, rate_orig)
-        else:
-            stop = None
-
-        rate_orig, data = read_wave(file=path, channel=channel, start=start, stop=stop)
-
-        if rate and rate != rate_orig:
-            data = librosa.core.resample(data, orig_sr=rate_orig, target_sr=rate, res_type=resample_method)
-        else:
-            rate = rate_orig
-            
-        return cls(rate=rate, data=data, filename=os.path.basename(path), offset=offset)
-
-    @classmethod
-    def gaussian_noise(cls, rate, sigma, samples, filename="gaussian_noise"):
-        """ Generate Gaussian noise signal
-
-            Args:
-                rate: float
-                    Sampling rate in Hz
-                sigma: float
-                    Standard deviation of the signal amplitude
-                samples: int
-                    Length of the audio signal given as the number of samples
-                filename: str
-                    Meta-data string (optional)
-
-            Returns:
-                Instance of AudioSignal
-                    Audio signal sampling of Gaussian noise
-
-            Example:
-                >>> from ketos.audio_processing.audio import AudioSignal
-                >>> # create gaussian noise with sampling rate of 10 Hz, standard deviation of 2.0 and 1000 samples
-                >>> a = AudioSignal.gaussian_noise(rate=10, sigma=2.0, samples=1000)
-                >>> # show signal
-                >>> fig = a.plot()
-                >>> fig.savefig("ketos/tests/assets/tmp/audio_noise.png")
-
-                .. image:: ../../../../ketos/tests/assets/tmp/audio_noise.png
-        """        
-        assert sigma > 0, "sigma must be strictly positive"
-
-        y = np.random.normal(loc=0, scale=sigma, size=samples)
-        return cls(rate=rate, data=y, filename=filename)
-
-    @classmethod
-    def morlet(cls, rate, frequency, width, samples=None, height=1, displacement=0, dfdt=0, filename="morlet"):
-        """ Audio signal with the shape of the Morlet wavelet
-
-            Uses :func:`util.morlet_func` to compute the Morlet wavelet.
-
-            Args:
-                rate: float
-                    Sampling rate in Hz
-                frequency: float
-                    Frequency of the Morlet wavelet in Hz
-                width: float
-                    Width of the Morlet wavelet in seconds (sigma of the Gaussian envelope)
-                samples: int
-                    Length of the audio signal given as the number of samples (if no value is given, samples = 6 * width * rate)
-                height: float
-                    Peak value of the audio signal
-                displacement: float
-                    Peak position in seconds
-                dfdt: float
-                    Rate of change in frequency as a function of time in Hz per second.
-                    If dfdt is non-zero, the frequency is computed as 
-
-                        f = frequency + (time - displacement) * dfdt 
-
-                filename: str
-                    Meta-data string (optional)
-
-            Returns:
-                Instance of AudioSignal
-                    Audio signal sampling of the Morlet wavelet 
-
-            Examples:
-                >>> from ketos.audio_processing.audio import AudioSignal
-                >>> # create a Morlet wavelet with frequency of 3 Hz and 1-sigma width of envelope set to 2.0 seconds
-                >>> wavelet1 = AudioSignal.morlet(rate=100., frequency=3., width=2.0)
-                >>> # show signal
-                >>> fig = wavelet1.plot()
-                >>> fig.savefig("ketos/tests/assets/tmp/morlet_standard.png")
-
-                .. image:: ../../../../ketos/tests/assets/tmp/morlet_standard.png
-
-                >>> # create another wavelet, but with frequency increasing linearly with time
-                >>> wavelet2 = AudioSignal.morlet(rate=100., frequency=3., width=2.0, dfdt=0.3)
-                >>> # show signal
-                >>> fig = wavelet2.plot()
-                >>> fig.savefig("ketos/tests/assets/tmp/morlet_dfdt.png")
-
-                .. image:: ../../../../ketos/tests/assets/tmp/morlet_dfdt.png
-        """        
-        if samples is None:
-            samples = int(6 * width * rate)
-
-        N = int(samples)
-
-        # compute Morlet function at N equally spaced points
-        dt = 1. / rate
-        stop = (N-1.)/2. * dt
-        start = -stop
-        time = np.linspace(start, stop, N)
-        y = morlet_func(time=time, frequency=frequency, width=width, displacement=displacement, norm=False, dfdt=dfdt)        
-        y *= height
-        
-        return cls(rate=rate, data=np.array(y), filename=filename)
-
-    @classmethod
-    def cosine(cls, rate, frequency, duration=1, height=1, displacement=0, filename="cosine"):
-        """ Audio signal with the shape of a cosine function
-
-            Args:
-                rate: float
-                    Sampling rate in Hz
-                frequency: float
-                    Frequency of the Morlet wavelet in Hz
-                duration: float
-                    Duration of the signal in seconds
-                height: float
-                    Peak value of the audio signal
-                displacement: float
-                    Phase offset in fractions of 2*pi
-                filename: str
-                    Meta-data string (optional)
-
-            Returns:
-                Instance of AudioSignal
-                    Audio signal sampling of the cosine function 
-
-            Examples:
-                >>> from ketos.audio_processing.audio import AudioSignal
-                >>> # create a Cosine wave with frequency of 7 Hz
-                >>> cos = AudioSignal.cosine(rate=1000., frequency=7.)
-                >>> # show signal
-                >>> fig = cos.plot()
-                >>> fig.savefig("ketos/tests/assets/tmp/cosine_audio.png")
-
-                .. image:: ../../../../ketos/tests/assets/tmp/cosine_audio.png
-        """        
-        N = int(duration * rate)
-
-        # compute cosine function at N equally spaced points
-        dt = 1. / rate
-        stop = (N-1.)/2. * dt
-        start = -stop
-        time = np.linspace(start, stop, N)
-        x = (time * frequency + displacement) * 2 * np.pi
-        y = height * np.cos(x)
-        
-        return cls(rate=rate, data=np.array(y), filename=filename)
-
     def get_data(self, id=0):
         """ Get the underlying data numpy array.
 
             Args:
                 id: int
-                    Audio signal ID. Only relevant if the AudioSignal object 
-                    contains multiple, stacked audio signals.
+                    Data set ID. Only relevant if the object 
+                    contains multiple, stacked data sets.
 
             Returns:
                 d: numpy array
                     Data
         """
-        if id is None or np.ndim(self.data) == 1:
+        if id is None or np.ndim(self.data) == self.ndim:
             d = self.data
         else:
             d = self.data[:,id]
 
         return d
+
+    def time_res():
+        """ Get the time resolution.
+
+            Returns:
+                : float
+                    Time resolution in seconds
+        """
+        return self.time_ax.bin_width()
 
     def segment(self, window, step=None):
         """ Divide the time axis into segments of uniform length, which may or may 
@@ -321,7 +172,7 @@ class AudioSignal():
             Window length and step size are converted to the nearest integer number 
             of time steps.
 
-            If necessary, the audio signal will be padded with zeros at the end to 
+            If necessary, the data array will be padded with zeros at the end to 
             ensure that all segments have an equal number of samples. 
 
             Args:
@@ -331,8 +182,8 @@ class AudioSignal():
                     Step size in seconds.
 
             Returns:
-                segs: AudioSignal
-                    Stacked audio signals
+                d: TimeData
+                    Stacked data segments
         """              
         if step_size is None:
             step_size = window_size
@@ -341,8 +192,8 @@ class AudioSignal():
         win_len = ap.num_samples(window, 1. / time_res)
         step_len = ap.num_samples(step, 1. / time_res)
 
-        # segment audio signal
-        segs = ap.segment(x=self.image, win_len=win_len, step_len=step_len, pad_mode='zero')
+        # segment data array
+        segs = ap.segment(x=self.data, win_len=win_len, step_len=step_len, pad_mode='zero')
 
         window = win_len * time_res
         step = step_len * time_res
@@ -357,9 +208,11 @@ class AudioSignal():
         # compute offsets
         offset = np.arange(num_segs) * step
 
-        audio = self.__class__(rate=self.rate, data=segs, filename=self.filename, offset=offset, label=self.label, annot=annots)
+        # create stacked TimeData object
+        d = self.__class__(data=segs, time_res=time_res, ndim=self.ndim, filename=self.filename,\
+            offset=offset, label=self.label, annot=annots)
 
-        return audio
+        return d
 
     def to_wav(self, path, auto_loudness=True):
         """ Save audio signal to wave file
