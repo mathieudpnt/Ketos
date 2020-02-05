7fa949661bb036215c8f8630105f87d9090f93e3
Fabio Frazao <fsfrazao@gmail.com>
2020-02-05 14:03:30 -0400

    test_resnet.py-TST-> create test module.
    Former-commit-id: d9f9ebd50fdce0191ccedb854dc0a2eaf291a0fd
    Former-commit-id: 4e4aaf81eeae132b05b19bc059e1f43d60523515
    Former-commit-id: 7c591fde868e485b99608e28db36ecde8a53d250
    Former-commit-id: be86b336e748eeeb7774bcca36daaee8009ec72e

---
+++ ketos/tests/neural_networks/test_resnet.py
@@ -0,0 +1,15 @@
+import pytest
+import numpy as np
+import tensorflow as tf
+from ketos.neural_networks.resnet import ResNetBlock, ResNetArch, ResNetInterface
+from ketos.neural_networks.losses import FScoreLoss
+from ketos.neural_networks.metrics import Precision, Recall, Accuracy, FScore
+from ketos.data_handling.data_feeding import BatchGenerator
+import os
+import tables
+import json
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+path_to_assets = os.path.join(os.path.dirname(current_dir),"assets")
+path_to_tmp = os.path.join(path_to_assets,'tmp')
+
