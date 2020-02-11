f1f2715c089724f58c9a9b38f0ff32b420d792d2
Fabio Frazao <fsfrazao@gmail.com>
2020-02-11 14:06:19 -0400

    cnn-TST-add recipe_simple fixture
    Former-commit-id: c587a64248756652e990d191e382de0d51a5bbd1
    Former-commit-id: 915be5e7798264a0fc2296fff7affd2b30f70607
    Former-commit-id: 6ce4d028ade2d71a29cbe7fa4d60278dfb1dc992
    Former-commit-id: 1829e6741069c58a16632db7d9989fd929730304

--- ketos/tests/neural_networks/test_cnn.py
+++ ketos/tests/neural_networks/test_cnn.py
@@ -15,3 +15,30 @@ import json
 current_dir = os.path.dirname(os.path.realpath(__file__))
 path_to_assets = os.path.join(os.path.dirname(current_dir),"assets")
 path_to_tmp = os.path.join(path_to_assets,'tmp')
+
+
+@pytest.fixture
+def recipe_simple_dict():
+    recipe = {'conv_set':[(64, False), (128, True), (256, True)],
+               'dense_set': [512, 256],
+               'n_classes':2,
+               'optimizer': {'name':'Adam', 'parameters': {'learning_rate':0.005}},
+               'loss_function': {'name':'FScoreLoss', 'parameters':{}},  
+               'metrics': [{'name':'CategoricalAccuracy', 'parameters':{}}]
+
+    }
+
+    return recipe
+
+
+@pytest.fixture
+def recipe_simple():
+    recipe = {'conv_set':[(64, False), (128, True), (256, True)],
+               'dense_set': [512, 256],
+               'n_classes':2,        
+               'optimizer': RecipeCompat('Adam', tf.keras.optimizers.Adam, learning_rate=0.005),
+               'loss_function': RecipeCompat('FScoreLoss', FScoreLoss),  
+               'metrics': [RecipeCompat('CategoricalAccuracy',tf.keras.metrics.CategoricalAccuracy)]
+        
+    }
+    return recipe
