c9841be25b5444de67eff96eb729dbcd288ea275
Fabio Frazao <fsfrazao@gmail.com>
2020-02-11 13:45:51 -0400

    cnn-TST-create test module
    Former-commit-id: 8a48c570a808d2eacc77d379989e425fc8050227
    Former-commit-id: 0de0912d6da88321fb4feddeca7eb1746c76b944
    Former-commit-id: 5ff202000a4949206cd1a8f2fda895c1a1a6783a
    Former-commit-id: 0f4dbfbb14a5771a9f1a9a508c66e53fb8af0d45

---
+++ ketos/tests/neural_networks/test_cnn.py
@@ -0,0 +1,17 @@
+import pytest
+import numpy as np
+import tensorflow as tf
+from ketos.neural_networks.nn_interface import RecipeCompat
+from ketos.neural_networks.resnet import CNNArch, CNNInterface
+from ketos.neural_networks.losses import FScoreLoss
+from ketos.neural_networks.metrics import Precision, Recall, Accuracy, FScore
+from ketos.data_handling.data_feeding import BatchGenerator
+import os
+import tables
+import json
+
+
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+path_to_assets = os.path.join(os.path.dirname(current_dir),"assets")
+path_to_tmp = os.path.join(path_to_assets,'tmp')
