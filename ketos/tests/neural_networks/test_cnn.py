9aedd715ab35a3d4ce01d0f85676f1647320fb7a
Fabio Frazao <fsfrazao@gmail.com>
2020-02-11 14:07:52 -0400

    cnn-TST-add recipe_detailed_dict fixture
    Former-commit-id: d813df923b53ee00ba6eb97cbabe9aa9250e6043
    Former-commit-id: 9574bbfed53fb95563430d7fb4dd3117d185e669
    Former-commit-id: 7ddd564e685b27c7dcb6d825fa18f2a69c7a19ae
    Former-commit-id: 8b97e438ede08c93fc5bda52d08f8f5f927047e1

--- ketos/tests/neural_networks/test_cnn.py
+++ ketos/tests/neural_networks/test_cnn.py
@@ -42,3 +42,24 @@ def recipe_simple():
         
     }
     return recipe
+
+
+@pytest.fixture
+def recipe_detailed_dict():
+    recipe = {'convolutional_layers':  [{'n_filters':96, "filter_shape":(11,11), 'strides':4, 'padding':'valid',  'activation':'relu', 'max_pool': {'pool_size':(3,3) , 'strides':(2,2)}, 'batch_normalization':True, },
+                                    {'n_filters':256, "filter_shape":(5,5), 'strides':1, 'padding':'valid', 'activation':'relu', 'max_pool': {'pool_size':(3,3) , 'strides':(2,2)}, 'batch_normalization':True, },
+                                    {'n_filters':384, "filter_shape":(3,3), 'strides':1, 'padding':'valid', 'activation':'relu', 'max_pool':None, 'batch_normalization':True,},
+                                    {'n_filters':384, "filter_shape":(3,3), 'strides':1, 'padding':'valid', 'activation':'relu', 'max_pool':None, 'batch_normalization':True,},
+                                    {'n_filters':256, "filter_shape":(3,3), 'strides':1, 'padding':'valid', 'activation':'relu', 'max_pool':{'pool_size':(3,3) , 'strides':(2,2)}, 'batch_normalization':True,},],
+                  
+              'dense_layers':[{'n_hidden':4096, 'activation':'relu', 'batch_normalization':True, 'dropout':0.5},
+                                    {'n_hidden':4096, 'activation':'relu', 'batch_normalization':True, 'dropout':0.5},
+                                    {'n_hidden':1000, 'activation':'relu', 'batch_normalization':True, 'dropout':0.5,}]
+               'n_classes':2,
+               'optimizer': {'name':'Adam', 'parameters': {'learning_rate':0.005}},
+               'loss_function': {'name':'FScoreLoss', 'parameters':{}},  
+               'metrics': [{'name':'CategoricalAccuracy', 'parameters':{}}]
+
+    }
+
+    return recipe
