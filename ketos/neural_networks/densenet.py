91e2c3c8ce4e373b1abcfed5549dfef9ac922d4a
Fabio Frazao <fsfrazao@gmail.com>
2020-06-30 09:36:17 -0300

    densenet.py-ENH-DenseNetInterface->Add class to build a densenet with the standard Ketos interface
    Former-commit-id: 1b8d9ec0736a805fbaef5f2b0d66028b45d15f7c
    Former-commit-id: f52b2faeca56382a3da3007eac271aba091464e5
    Former-commit-id: 380a5661df1a57f2e6cd1e007fcae726e98479d0
    Former-commit-id: 32afd50ca98d7b454605400b5a4134abfd7c9ce3

--- ketos/neural_networks/densenet.py
+++ ketos/neural_networks/densenet.py
@@ -24,11 +24,15 @@
 #       along with this program.  If not, see <https://www.gnu.org/licenses/>.     #
 # ================================================================================ #
 
-""" densenet sub-module within the ketos.neural_networks module
+"""densenet sub-module within the ketos.neural_networks module
 
     This module provides classes to implement Residual Networks (ResNets).
 
-    Contents:
+    Contents: ConvBlock class
+              DenseBlock class
+              TransitionBlock class
+              DenseNetArch class
+              DenseNetInterface class
         
 
 """
@@ -148,7 +152,7 @@ class DenseNetArch(tf.keras.Model):
             growth_rate:int
                 The factor by which the number of filters (i.e.: channels) within each dense block grows.
             compression_factor: float
-                The factor by which transition blocks reduce the number of filters (i.e.: channels) between dense blocks.
+                The factor by which transition blocks reduce the number of filters (i.e.: channels) between dense blocks (between 0 and 1).
             dropout_rate: float
                 The droput rate (between 0 and 1) used in each transition block. Use 0 for no dropout.
             n_classes:int
@@ -200,5 +204,176 @@ class DenseNetArch(tf.keras.Model):
 
         return outputs
 
+
+
+default_densenet_recipe =  {'dense_blocks':[ 6, 12, 24, 16],
+                    'growth_rate':32,
+                    'compression_factor':0.5,
+                    'n_classes':2,
+                    'dropout_rate':0.2,
+                    'optimizer': RecipeCompat('Adam', tf.keras.optimizers.Adam, learning_rate=0.005),
+                    'loss_function': RecipeCompat('CategoricalCrossentropy', tf.keras.losses.CategoricalCrossentropy),  
+                    'metrics': [RecipeCompat('BinaryAccuracy',tf.keras.metrics.BinaryAccuracy),
+                                RecipeCompat('Precision',tf.keras.metrics.Precision, class_id=1),
+                                RecipeCompat('Recall',tf.keras.metrics.Recall, class_id=1)],
+                    }
+
+
+
+class DenseNetInterface(NNInterface):
+
+
+    @classmethod
+    def _build_from_recipe(cls, recipe, recipe_compat=True):
+        """ Build a DenseNet model from a recipe.
+
+            Args:
+                recipe: dict
+                    A recipe dictionary. optimizer, loss function
+                    and metrics must be instances of ketos.neural_networks.RecipeCompat.
+                    
+                    Example recipe:
+                    
+                    >>> {'dense_blocks': [6, 12, 24, 16], # doctest: +SKIP
+                    ...    'growth_rate':32,
+                    ...    'compression_factor':0.5,
+                    ...    'n_classes':2,
+                    ...    'dropout_rate':0.2,        
+                    ...    'optimizer': RecipeCompat('Adam', tf.keras.optimizers.Adam, learning_rate=0.005),
+                    ...    'loss_function': RecipeCompat('BinaryCrossentropy', tf.keras.losses.BinaryCrossentropy),  
+                    ...    'metrics': [RecipeCompat('CategoricalAccuracy',tf.keras.metrics.CategoricalAccuracy)],
+                    }
+                     
+
+            Returns:
+                An instance of ResNetInterface.
+
+        """
+
+        dense_blocks = recipe['dense_blocks']
+        growth_rate = recipe['growth_rate']
+        compression_factor = recipe['compression_factor']
+        n_classes = recipe['n_classes']
+        dropout_rate = recipe['dropout_rate']
+        
+        
+        if recipe_compat == True:
+            optimizer = recipe['optimizer']
+            loss_function = recipe['loss_function']
+            metrics = recipe['metrics']
+            
+        else:
+            optimizer = cls._optimizer_from_recipe(recipe['optimizer'])
+            loss_function = cls._loss_function_from_recipe(recipe['loss_function'])
+            metrics = cls._metrics_from_recipe(recipe['metrics'])
+            
+
+        instance = cls(dense_blocks=dense_blocks, growth_rate=growth_rate, compression_factor=compression_factor, n_classes=n_classes, dropout_rate=dropout_rate, optimizer=optimizer, loss_function=loss_function, metrics=metrics)
+
+        return instance
+
+    @classmethod
+    def _read_recipe_file(cls, json_file, return_recipe_compat=True):
+        """ Read a DenseNet recipe saved in a .json file.
+
+            Args:
+                json_file:string
+                    Full path (including filename and extension) to the .json file containing the recipe.
+                return_recipe_compat:bool
+                    If True, returns a dictionary where the optimizer, loss_function, metrics and 
+                    secondary_metrics (if available) values are instances of the ketos.neural_networks.nn_interface.RecipeCompat.
+                    The returned dictionary will be equivalent to:
+                            
+                            >>> {'dense_blocks': [6, 12, 24, 16], # doctest: +SKIP
+                            ...  'growth_rate':32,
+                            ...  'compression_factor':0.5,
+                            ...  'n_classes':2,
+                            ...  'dropout_rate':0.2,        
+                            ...  'optimizer': RecipeCompat('Adam', tf.keras.optimizers.Adam, learning_rate=0.005),
+                            ...  'loss_function': RecipeCompat('BinaryCrossentropy', tf.keras.losses.BinaryCrossentropy),  
+                            ...  'metrics': [RecipeCompat('CategoricalAccuracy',tf.keras.metrics.CategoricalAccuracy)],
+                            }
+
+                    If False, the optimizer, loss_function, metrics and secondary_metrics (if available) values will contain a
+                    dictionary representation of such fields instead of the RecipeCompat objects:
+                            >>> {'dense_blocks': [6, 12, 24, 16], # doctest: +SKIP
+                            ...  'growth_rate':32,
+                            ...  'compression_factor':0.5,
+                            ...  'n_classes':2,
+                            ...  'dropout_rate':0.2,        
+                            ...  'optimizer': {'name':'Adam', 'parameters': {'learning_rate':0.005}},
+                            ...  'loss_function': {'name':'BinaryCrossentropy', 'parameters':{}},  
+                            ...  'metrics': [{'name':'CategoricalAccuracy', 'parameters':{}}]}
+
+                Returns:
+                    recipe, according to 'return_recipe_compat.
+
+        """
+
+        with open(json_file, 'r') as json_recipe:
+            recipe_dict = json.load(json_recipe)
+
+        optimizer = cls._optimizer_from_recipe(recipe_dict['optimizer'])
+        loss_function = cls._loss_function_from_recipe(recipe_dict['loss_function'])
+        metrics = cls._metrics_from_recipe(recipe_dict['metrics'])
+        
+        if return_recipe_compat == True:
+            recipe_dict['optimizer'] = optimizer
+            recipe_dict['loss_function'] = loss_function
+            recipe_dict['metrics'] = metrics
+            
+        else:
+            recipe_dict['optimizer'] = cls._optimizer_to_recipe(optimizer)
+            recipe_dict['loss_function'] = cls._loss_function_to_recipe(loss_function)
+            recipe_dict['metrics'] = cls._metrics_to_recipe(metrics)
+        
+        # recipe_dict['block_sets'] = recipe_dict['block_sets']
+        # recipe_dict['n_classes'] = recipe_dict['n_classes']
+        # recipe_dict['initial_filters'] = recipe_dict['initial_filters']
+
+        return recipe_dict
+
+    def __init__(self, dense_blocks=default_densenet_recipe['dense_blocks'], growth_rate=default_densenet_recipe['growth_rate'], compression_factor=default_densenet_recipe['compression_factor'], n_classes=default_densenet_recipe['n_classes'], dropout_rate=default_densenet_recipe['dropout_rate'], optimizer=default_densenet_recipe['optimizer'], loss_function=default_densenet_recipe['loss_function'], metrics=default_densenet_recipe['metrics']):
+        super(DenseNetInterface, self).__init__(optimizer=optimizer, loss_function=loss_function, metrics=metrics)
+
+        self.dense_blocks = dense_blocks
+        self.growth_rate = growth_rate
+        self.compression_factor = compression_factor
+        self.n_classes = n_classes
+        self.dropout_rate = dropout_rate
+
+        self.model = DenseNetArch(dense_blocks = self.dense_blocks, growth_rate=self.growth_rate, compression_factor=self.compression_factor, n_classes=self.n_classes, dropout_rate=self.dropout_rate)
+
+    def _extract_recipe_dict(self):
+        """ Create a recipe dictionary from a DenseNetInterface instance.
+
+            The resulting recipe contains all the fields necessary to build the same network architecture used by the instance calling this method.
+            
+            Returns:
+                recipe:dict
+                    A dictionary containing the recipe fields necessary to build the same network architecture.
+                    The output is equivalent to:
+                       >>> {'dense_blocks': [6, 12, 24, 16], # doctest: +SKIP
+                        ...  'growth_rate':32,
+                        ...  'compression_factor':0.5,
+                        ...  'n_classes':2,
+                        ...  'dropout_rate':0.2,        
+                        ...  'optimizer': RecipeCompat('Adam', tf.keras.optimizers.Adam, learning_rate=0.005),
+                        ...  'loss_function': RecipeCompat('BinaryCrossentropy', tf.keras.losses.BinaryCrossentropy),  
+                        ...  'metrics': [RecipeCompat('CategoricalAccuracy',tf.keras.metrics.CategoricalAccuracy)],
+                        }
+        """
+
+        recipe = {}
+        recipe['dense_blocks'] = self.dense_blocks
+        recipe['growth_rate'] = self.growth_rate
+        recipe['compression_factor'] = self.compression_factor
+        recipe['n_classes'] = self.n_classes
+        recipe['dropout_rate'] = self.dropout_rate
+        recipe['optimizer'] = self._optimizer_to_recipe(self.optimizer)
+        recipe['loss_function'] = self._loss_function_to_recipe(self.loss_function)
+        recipe['metrics'] = self._metrics_to_recipe(self.metrics)
+        
+        return recipe
 
 
