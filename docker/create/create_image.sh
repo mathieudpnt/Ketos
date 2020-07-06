24823394e6e261517b1013db21b2eb1cb5bc0a43
Fabio Frazao <fsfrazao@gmail.com>
2020-07-06 08:41:15 -0300

    Change version to 2.0.1
    Former-commit-id: 3f84490ebe7a6484c7c4807e32006a127b941e13
    Former-commit-id: cda71c7c13674bcbd2088bf16609f639552c4f70
    Former-commit-id: 281ff397940b3577ce6ad68c92b1595535d80728
    Former-commit-id: f9431c60a3a7df1c760c20b591d421f36b503656

--- docker/create/create_image.sh
+++ docker/create/create_image.sh
@@ -6,13 +6,13 @@ cp ../../requirements.txt .
 cp ../../../sphinx_mer_rtd_theme-0.4.3.dev0.tar.gz .
 
 # build image
-docker build --tag=ketos_v2.0.0b5 .
+docker build --tag=ketos_v2.0.1 .
 
 # tag image
-docker tag ketos_v2.0.0b5 meridiancfi/ketos:v2.0.0b5
+docker tag ketos_v2.0.1 meridiancfi/ketos:v2.0.1
 
 # push image to repository
-docker push meridiancfi/ketos:v2.0.0b5
+docker push meridiancfi/ketos:v2.0.1
 
 # clean
 rm -rf ketos
