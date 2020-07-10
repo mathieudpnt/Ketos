c95e7efd624cfb228971e17c02dd638d54a0a965
Fabio Frazao <fsfrazao@gmail.com>
2020-07-10 17:01:06 -0300

    test_densenet.py-eng-Add recipe fixtures
    Former-commit-id: 494c39629f6bb2a613560251e7f6ad2b29e797ad
    Former-commit-id: e9b6cd175eb942d39851f741cf9ca84b1caffa2b
    Former-commit-id: 91a356fada22fd897cc6307f61e68f7743a32126
    Former-commit-id: fd074d192346a665eedfa4be39d64592d1a0bafb

--- ketos/tests/neural_networks/test_densenet.py
+++ ketos/tests/neural_networks/test_densenet.py
@@ -14,3 +14,30 @@ path_to_assets = os.path.join(os.path.dirname(current_dir),"assets")
 path_to_tmp = os.path.join(path_to_assets,'tmp')
 
 
+@pytest.fixture
+def recipe_dict():
+    recipe = {'dense_blocks':[ 6, 12, 24, 16],
+                    'growth_rate':32,
+                    'compression_factor':0.5,
+                    'n_classes':2,
+                    'dropout_rate':0.2,
+                    'optimizer': {'recipe_name':'Adam', 'parameters': {'learning_rate':0.005}},
+                    'loss_function': {'recipe_name':'CategoricalCrossEntropy', 'parameters':{}},  
+                    'metrics': [{'recipe_name':'CategoricalAccuracy', 'parameters':{}}]
+        
+    }
+    return recipe
+    
+@pytest.fixture
+def recipe():
+    recipe = {'dense_blocks':[ 6, 12, 24, 16],
+                    'growth_rate':32,
+                    'compression_factor':0.5,
+                    'n_classes':2,
+                    'dropout_rate':0.2,
+                    'optimizer': RecipeCompat('Adam', tf.keras.optimizers.Adam, learning_rate=0.005),
+                    'loss_function': RecipeCompat('CategoricalCrossentropy', tf.keras.losses.CategoricalCrossentropy),  
+                    'metrics': [RecipeCompat('CategoricalAccuracy',tf.keras.metrics.CategoricalAccuracy),
+                                ],
+                    }
+    return recipe
