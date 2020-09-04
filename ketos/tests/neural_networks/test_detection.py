2e841b8df5549f1c468d548609a59848f3d9a091
Fabio Frazao <fsfrazao@gmail.com>
2020-09-04 15:58:05 -0300

    test_detections-TST-test_compute_avg_score-Add test cases
    Former-commit-id: 75772a6ce96d95860c52863156eeddc2b91f64a6
    Former-commit-id: 9041b0165eda97acf492e097c078ce81783d218d
    Former-commit-id: 13d3475c573e72ace7be37022326efae2d4923a4
    Former-commit-id: 03ec4846e7c6dce6ddf6a9d42bd65f35198b635a

--- ketos/tests/neural_networks/test_detection.py
+++ ketos/tests/neural_networks/test_detection.py
@@ -1,8 +1,7 @@
 
 import pytest
-from ketos.neural_networks.dev_utils.nn_interface import RecipeCompat, NNInterface
-from ketos.neural_networks.dev_utils.losses import FScoreLoss
-from ketos.data_handling.data_feeding import BatchGenerator
+from ketos.neural_networks.dev_utils.detection import *
+import numpy as np
 import os
 import shutil
 import tables
@@ -14,8 +13,13 @@ path_to_assets = os.path.join(os.path.dirname(current_dir),"assets")
 path_to_tmp = os.path.join(path_to_assets,'tmp')
 
 
-def test_compute_avg_score():
-    pass
+@pytest.mark.parametrize("score_vector, win_len,  expected", [(np.array([1.,1.,1.,1.,1.,1.]), 3, np.array([0.,1.,1.,1.,1.,0.])),
+                                                                (np.array([0.,0.,0.,0.,0.,0.]), 3, np.array([0.,0.,0.,0.,0.,0.])),])
+def test_compute_avg_score(score_vector, win_len, expected):
+    avg = compute_avg_score(score_vector, win_len)
+    assert len(avg) == len(score_vector)
+    assert np.array_equal(avg, expected)
+
 
 def test_match_detection_to_time():
     pass
