12e2abc6ec31c8de26c6cfbaa2d776fd0ae14de6
Fabio Frazao <fsfrazao@gmail.com>
2020-09-08 21:15:30 -0300

    test_detection.py-TST-Add tests to compute avg_score and map_detection_to_time
    Former-commit-id: 7b34be760cc0b54d98ffb86e260dd5223d037261
    Former-commit-id: be2030f6a29fecc61bcbfc29459b749f5f424332
    Former-commit-id: c097348d7823ba9bb7a6e26b490b2ac70ad1d47a
    Former-commit-id: 56121da00b36f96a95cfafa655ed918fc0b8c9d5

--- ketos/tests/neural_networks/test_detection.py
+++ ketos/tests/neural_networks/test_detection.py
@@ -14,15 +14,45 @@ path_to_tmp = os.path.join(path_to_assets,'tmp')
 
 
 @pytest.mark.parametrize("score_vector, win_len,  expected", [(np.array([1.,1.,1.,1.,1.,1.]), 3, np.array([0.,1.,1.,1.,1.,0.])),
-                                                                (np.array([0.,0.,0.,0.,0.,0.]), 3, np.array([0.,0.,0.,0.,0.,0.])),])
+                                                                (np.array([0.,0.,0.,0.,0.,0.]), 3, np.array([0.,0.,0.,0.,0.,0.])),
+                                                                (np.array([1.,1.,1.,1.,1.,1.]), 5, np.array([0.,0.,1.,1.,0.,0.])),
+                                                                (np.array([0.,0.,0.,0.,0.,0.]), 3, np.array([0.,0.,0.,0.,0.,0.])),
+                                                                (np.array([0.1,0.35,0.7,0.8,0.5,0.3,0.2,0,0.89,0.5]), 3, np.array([0,0.3833,0.6166,0.6666,0.5333,0.3333,0.1666,0.3633,0.4633,0])),
+                                                                 
+                                                                 ])
 def test_compute_avg_score(score_vector, win_len, expected):
     avg = compute_avg_score(score_vector, win_len)
     assert len(avg) == len(score_vector)
-    assert np.array_equal(avg, expected)
+    assert np.allclose(avg, expected,  rtol=1e-03)
 
 
-def test_match_detection_to_time():
-    pass
+def test_compute_avg_score_raise_exception():
+    with pytest.raises(AssertionError):
+        avg = compute_avg_score(np.array([1,1,1,1,1]), win_len=2)
+
+
+@pytest.mark.parametrize("det_start, det_end, batch_start_timestamp, batch_end_timestamp, step, spec_dur, buffer, expected", 
+                        [(0,1,300,600,0.5,3.0, 1.0, (300,2.0)),
+                         (0,2,300,600,0.5,3.0, 1.0, (300,2.5)),
+                         (0,3,300,600,0.5,3.0, 1.0, (300,3.0)),
+                         (1,4,300,600,0.5,3.0, 1.0, (300,3.5)),
+                         (2,5,300,600,0.5,3.0, 1.0, (300,4.0)),
+                         (2,6,300,600,0.5,3.0, 1.0, (300,4.5)),
+                         (3,7,300,600,0.5,3.0, 1.0, (300.5,4.5)),
+                         (4,8,300,600,0.5,3.0, 1.0, (301,4.5)),
+                         (1,2,300,600,0.5,3.0, 1.0, (300,2.5)),
+                         (2,2,300,600,0.5,3.0, 1.0, (300.0,2.5)),
+                         (20,25,300,600,0.5,3.0, 1.0, (309.0,5.0)),
+                         ])
+def test_map_detection_to_time(det_start, det_end, batch_start_timestamp, batch_end_timestamp, step, spec_dur, buffer, expected):
+    mapped_detection = map_detection_to_time(det_start, det_end, batch_start_timestamp, batch_end_timestamp, step, spec_dur, buffer)
+    assert mapped_detection == expected
+
+def test_map_detection_to_time_det_end_exception():
+    with pytest.raises(ValueError):
+        map_detection_to_time(det_start=10, det_end=9, batch_start_timestamp=300, batch_end_timestamp=600, step=0.5, spec_dur=3.0, buffer=1)
+
+
 
 def test_group_detections():
     pass
