f4320d400c1db22fe41ef2d30e7ef1c19b19ed4a
oliver <oliver.kirsebom@gmail.com>
2022-04-07 17:03:36 -0300

    updated docs port server

--- docs/upload_docs.sh
+++ docs/upload_docs.sh
@@ -4,17 +4,17 @@
 echo 'Enter username'
 read user
 
-ssh -p 22 $user@206.12.88.81 << EOF
+ssh -p 23022 $user@206.12.88.81 << EOF
 sudo mkdir /var/www/html/ketos2
 sudo chown -h -R ubuntu:ubuntu /var/www/html/ketos2
 sudo chown -h -R ubuntu:ubuntu /var/www/html/ketos
 EOF
 
 # copy html folder
-scp -P 22 -r build/html/* $user@206.12.88.81:/var/www/html/ketos2
+scp -P 23022 -r build/html/* $user@206.12.88.81:/var/www/html/ketos2
 
 # replace old folder on server, and set permissions
-ssh -p 22 $user@206.12.88.81 << EOF
+ssh -p 23022 $user@206.12.88.81 << EOF
 cd /var/www/html/
 sudo rm -rf ketos
 sudo mv ketos2 ketos
